e5afeb6a776047956bdd959a4886af65f326dd87
Sword-Is-Cat <jhbsp@naver.com>
2020-04-14 02:21:12 +0900

    solve

---
+++ LeetCode/src/Easy/no581/Solution.java
@@ -0,0 +1,30 @@
+package Easy.no581;
+
+import java.util.Arrays;
+
+class Solution {
+	public int findUnsortedSubarray(int[] nums) {
+
+		int answer = 0;
+
+		int[] numz = nums.clone();
+		Arrays.sort(numz);
+
+		int i = 0;
+		for (; i < nums.length; i++) {
+			if (nums[i] != numz[i])
+				break;
+			if (i == nums.length - 1)
+				return 0;
+		}
+
+		int j = 0;
+		for (; j < nums.length; j++) {
+			if (nums[nums.length - 1 - j] != numz[nums.length - 1 - j])
+				break;
+		}
+
+		return nums.length - i - j;
+
+	}
+}
