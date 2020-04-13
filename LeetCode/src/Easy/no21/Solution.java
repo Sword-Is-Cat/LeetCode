406eaa3530efe7d0514746f9e5550959cfd3ad48
Sword-Is-Cat <jhbsp@naver.com>
2020-04-14 01:52:55 +0900

    solve

--- LeetCode/src/Easy/no21/Solution.java
+++ LeetCode/src/Easy/no21/Solution.java
@@ -7,27 +7,25 @@ package Easy.no21;
 //class Solution {
 //	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 //
-//		ListNode answer = new ListNode(0);
-//		ListNode l3 = answer;
-//
-//		while (l1 != null && l2 != null) {
-//			if (l1.val <= l2.val) {
-//				l3.next = l1;
+//		ListNode process = new ListNode(0);
+//		ListNode answer = process;
+//		
+//		while(l1!=null && l2!=null) {
+//			if(l1.val<=l2.val) {
+//				process.next = l1;
 //				l1 = l1.next;
+//				process = process.next;
 //			} else {
-//				l3.next = l2;
+//				process.next = l2;
 //				l2 = l2.next;
+//				process = process.next;
 //			}
-//
-//			l3 = l3.next;
 //		}
 //		
-//		if(l1 != null) {
-//			l3.next = l1;
-//		}
-//		if(l2 != null) {
-//			l3.next = l2;
-//		}
+//		if(l1 == null)
+//			process.next = l2;
+//		else
+//			process.next = l1;
 //		
 //		return answer.next;
 //	}
