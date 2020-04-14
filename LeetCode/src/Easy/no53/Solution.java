6781966a19f29ca6f7261b6c89852ce59917d293
Sword-Is-Cat <jhbsp@naver.com>
2020-04-15 00:11:50 +0900

    remove sout, code shorting

--- LeetCode/src/Easy/no53/Solution.java
+++ LeetCode/src/Easy/no53/Solution.java
@@ -6,24 +6,21 @@ class Solution {
 		if (nums.length == 1)
 			return nums[0];
 
-		int process = nums[0];
-		int max = nums[0];
+		int process = 0;
+		int max = 0;
 		int answer = 0;
-		
-		process = (process>0)?process:0;
-		answer = (answer>process)?answer:process;
 
-		for (int i = 1; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			process += nums[i];
-			max = (max>nums[i])?max:nums[i];
-			nums[i] = nums[i - 1] + nums[i];
-			process = (process>0)?process:0;
-			answer = (answer>process)?answer:process;
-			
-			System.out.println(process);
+			process = (process > 0) ? process : 0;
+			answer = (answer > process) ? answer : process;
+			if (i == 0)
+				max = nums[0];
+			else {
+				max = (max > nums[i]) ? max : nums[i];
+				nums[i] = nums[i - 1] + nums[i];
+			}
 		}
-		
-		return (answer>0)?answer:max;
+		return (answer > 0) ? answer : max;
 	}
-
 }
