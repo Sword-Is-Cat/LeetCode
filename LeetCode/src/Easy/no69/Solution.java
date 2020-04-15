02e69d2e2a826bc5c71426c5c4384caa177ad759
Sword-Is-Cat <jhbsp@naver.com>
2020-04-16 02:43:47 +0900

    sqrt is sqrt

--- LeetCode/src/Easy/no69/Solution.java
+++ LeetCode/src/Easy/no69/Solution.java
@@ -3,14 +3,6 @@ package Easy.no69;
 class Solution {
 	public int mySqrt(int x) {
 
-		long lo;
-		
-		for(int i = 0 ; ;i++) {
-			lo = (long) i * i;
-			if(lo > x)
-				return i-1;
-		}
-
+	return (int)Math.sqrt(x);
 	}
-	
 }
