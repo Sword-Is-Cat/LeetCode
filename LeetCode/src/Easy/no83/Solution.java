cadf07902129ba00acf70600af75d3c1fb8c05ac
Sword-Is-Cat <jhbsp@naver.com>
2020-04-17 01:23:15 +0900

    .

--- LeetCode/src/Easy/no83/Solution.java
+++ LeetCode/src/Easy/no83/Solution.java
@@ -8,21 +8,21 @@ package Easy.no83;
  *  ListNode(int x) { val = x; }
  * }
  */
-class Solution {
-	public ListNode deleteDuplicates(ListNode head) {
-
-		ListNode answer = head;
-
-		if (head == null || head.next == null)
-			return answer;
-
-		while (head.next != null) {
-			if (head.val == head.next.val)
-				head.next = head.next.next;
-			else
-				head = head.next;
-		}
-
-		return answer;
-	}
-}
+//class Solution {
+//	public ListNode deleteDuplicates(ListNode head) {
+//
+//		ListNode answer = head;
+//
+//		if (head == null || head.next == null)
+//			return answer;
+//
+//		while (head.next != null) {
+//			if (head.val == head.next.val)
+//				head.next = head.next.next;
+//			else
+//				head = head.next;
+//		}
+//
+//		return answer;
+//	}
+//}
