725ce9728bffba9e9776f106ed857296b655dffa
Sword-Is-Cat <jhbsp@naver.com>
2020-04-18 01:58:30 +0900

    comment

--- LeetCode/src/Easy/no107/Solution.java
+++ LeetCode/src/Easy/no107/Solution.java
@@ -12,34 +12,34 @@ package Easy.no107;
 import java.util.List;
 import java.util.ArrayList;
 
-class Solution {
-    
-    List<List<Integer>> dummy = new ArrayList<>();
-    
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        
-        process(root, 0);
-        
-        List<List<Integer>> answer = new ArrayList<>();
-        
-        for(int i = 0 ; i<dummy.size() ; i++){
-            answer.add(dummy.get(dummy.size()-1-i));
-        }
-        
-        return answer;
-        
-    }
-    
-    public void process (TreeNode node, int i){
-        if(node==null)
-            return;
-        
-        if(dummy.size() == i)
-            dummy.add(new ArrayList<Integer>());
-      
-        dummy.get(i).add(node.val);
-                
-        process(node.left, i+1);
-        process(node.right, i+1);
-    }
-}
+//class Solution {
+//    
+//    List<List<Integer>> dummy = new ArrayList<>();
+//    
+//    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//        
+//        process(root, 0);
+//        
+//        List<List<Integer>> answer = new ArrayList<>();
+//        
+//        for(int i = 0 ; i<dummy.size() ; i++){
+//            answer.add(dummy.get(dummy.size()-1-i));
+//        }
+//        
+//        return answer;
+//        
+//    }
+//    
+//    public void process (TreeNode node, int i){
+//        if(node==null)
+//            return;
+//        
+//        if(dummy.size() == i)
+//            dummy.add(new ArrayList<Integer>());
+//      
+//        dummy.get(i).add(node.val);
+//                
+//        process(node.left, i+1);
+//        process(node.right, i+1);
+//    }
+//}
