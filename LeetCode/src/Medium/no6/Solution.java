3a6b231e5ad897756340dec5aa20d1a7d01c4aa6
HongBum(home) <jhbsp@naver.com>
2020-07-15 20:33:44 +0900

    update

--- LeetCode/src/Medium/no6/Solution.java
+++ LeetCode/src/Medium/no6/Solution.java
@@ -1,34 +1,34 @@
 package Medium.no6;
 
 class Solution {
-    public String convert(String s, int numRows) {
-        
-        if(numRows == 1)
-            return s;
-        
-        StringBuilder[] sbArr = new StringBuilder[numRows];
-        
-        for(int i = 0 ; i<sbArr.length ; i++){
-            sbArr[i] = new StringBuilder();
-        }
-        
-        char[] chArr = s.toCharArray();
-        
-        for(int i = 0 ; i<chArr.length ; i++){
-            int mod = i%(numRows*2-2);
-            if(mod<numRows-1)
-                sbArr[mod].append(chArr[i]);
-            else
-                sbArr[numRows*2-2-mod].append(chArr[i]);
-        }
-        
-        StringBuilder answer = new StringBuilder();
-        
-        for(int i = 0 ; i<sbArr.length ; i++){
-            answer.append(sbArr[i]);
-        }
-        
-        return answer.toString();
-        
-    }
+	public String convert(String s, int numRows) {
+
+		if (numRows == 1)
+			return s;
+
+		StringBuilder[] sbArr = new StringBuilder[numRows];
+
+		for (int i = 0; i < sbArr.length; i++) {
+			sbArr[i] = new StringBuilder();
+		}
+
+		char[] chArr = s.toCharArray();
+
+		for (int i = 0; i < chArr.length; i++) {
+			int mod = i % (numRows * 2 - 2);
+			if (mod < numRows - 1)
+				sbArr[mod].append(chArr[i]);
+			else
+				sbArr[numRows * 2 - 2 - mod].append(chArr[i]);
+		}
+
+		StringBuilder answer = new StringBuilder();
+
+		for (int i = 0; i < sbArr.length; i++) {
+			answer.append(sbArr[i]);
+		}
+
+		return answer.toString();
+
+	}
 }
