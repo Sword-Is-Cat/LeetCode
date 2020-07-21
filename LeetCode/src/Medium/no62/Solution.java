31e5f8218d5f7f518dd18031c9b52d6879276754
HongBum(home) <jhbsp@naver.com>
2020-07-21 12:07:11 +0900

    solve

---
+++ LeetCode/src/Medium/no62/Solution.java
@@ -0,0 +1,23 @@
+package Medium.no62;
+
+class Solution {
+	public int uniquePaths(int m, int n) {
+
+		m--;
+		n--;
+
+		long head = 1;
+		long tail = 1;
+		int min = Math.min(m, n);
+
+		for (int i = 0; i < min; i++) {
+			head *= m + n - i;
+			tail *= min - i;
+		}
+
+		long answer = head / tail;
+
+		return (int) answer;
+
+	}
+}
