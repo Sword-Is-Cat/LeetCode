8b5aa58130ca83e9c3d7b9a34b461b87bf209d67
HongBum(home) <jhbsp@naver.com>
2020-07-27 13:18:14 +0900

    solve

---
+++ LeetCode/src/Easy/no191/Solution.java
@@ -0,0 +1,17 @@
+package Easy.no191;
+
+public class Solution {
+	// you need to treat n as an unsigned value
+	public int hammingWeight(int n) {
+
+		int answer = 0;
+
+		for (int i = 0; i < 32; i++) {
+			answer += n % 2;
+			n >>= 1;
+		}
+
+		return answer > 0 ? answer : -answer;
+
+	}
+}
