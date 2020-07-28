22d43387bcb539c4607f24b9801d341285b0ee0f
HongBum(home) <jhbsp@naver.com>
2020-07-28 13:06:34 +0900

    solve

---
+++ LeetCode/src/Easy/no303/NumArray.java
@@ -0,0 +1,29 @@
+package Easy.no303;
+
+class NumArray {
+
+	int[] sums;
+
+	public NumArray(int[] nums) {
+
+		sums = new int[nums.length];
+		int sum = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			sums[i] = sum;
+		}
+
+	}
+
+	public int sumRange(int i, int j) {
+
+		return i == 0 ? sums[j] : sums[j] - sums[i - 1];
+
+	}
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such: NumArray obj =
+ * new NumArray(nums); int param_1 = obj.sumRange(i,j);
+ */
