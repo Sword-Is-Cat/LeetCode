61094302b5e2738beb68f5b4a24f7d2f5b211a26
HongBum(home) <jhbsp@naver.com>
2020-07-29 14:14:48 +0900

    solve

---
+++ LeetCode/src/Easy/no345/Solution.java
@@ -0,0 +1,37 @@
+package Easy.no345;
+
+class Solution {
+	public String reverseVowels(String s) {
+
+		char[] arr = s.toCharArray();
+
+		int i = 0;
+		int j = arr.length - 1;
+
+		while (i < j) {
+			if (checkVowel(arr[i]) && checkVowel(arr[j])) {
+				char temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			} else {
+				while (i < arr.length && !checkVowel(arr[i]))
+					i++;
+				while (j >= 0 && !checkVowel(arr[j]))
+					j--;
+			}
+		}
+
+		return new String(arr);
+
+	}
+
+	public boolean checkVowel(char ch) {
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+				|| ch == 'O' || ch == 'U')
+			return true;
+		else
+			return false;
+	}
+}
