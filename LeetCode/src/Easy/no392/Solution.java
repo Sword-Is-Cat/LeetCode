711171a6189c5d7f98a51c0b384ab2ff41734853
HongBum(home) <jhbsp@naver.com>
2020-07-30 13:35:24 +0900

    solve

---
+++ LeetCode/src/Easy/no392/Solution.java
@@ -0,0 +1,30 @@
+package Easy.no392;
+
+class Solution {
+	public boolean isSubsequence(String s, String t) {
+
+		if (s.length() == 0)
+			return true;
+
+		boolean answer = false;
+
+		char[] sArr = s.toCharArray();
+		char[] tArr = t.replaceAll("[^" + s + "]", "").toCharArray();
+
+		int i = 0;
+
+		for (char ch : tArr) {
+
+			if (sArr[i] == ch)
+				i++;
+
+			if (i == sArr.length) {
+				answer = true;
+				break;
+			}
+		}
+
+		return answer;
+
+	}
+}
