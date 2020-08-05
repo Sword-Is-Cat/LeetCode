856bbec6d6386cc1a29ec51fd702226e7b9065c0
jang <jhbsp@naver.com>
2020-08-05 14:58:28 +0900

    solve

---
+++ LeetCode/src/Challenge/August_3/Solution.java
@@ -0,0 +1,17 @@
+package Challenge.August_3;
+
+class Solution {
+	public boolean isPowerOfFour(int num) {
+
+		boolean answer = false;
+
+		int root = (int) Math.sqrt(num);
+
+		if (num != 0)
+			if (root * root == num && (Integer.MAX_VALUE / 2 + 1) % root == 0)
+				answer = true;
+
+		return answer;
+
+	}
+}
