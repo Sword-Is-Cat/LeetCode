4cb305f8514fb0f3ade1e6b6494009b72d24ed7d
jang <jhbsp@naver.com>
2020-08-05 15:11:51 +0900

    solve

---
+++ LeetCode/src/Easy/no557/Solution.java
@@ -0,0 +1,23 @@
+package Easy.no557;
+
+class Solution {
+    public String reverseWords(String s) {
+    	
+    	String[] arr = s.split(" ");
+    	
+    	StringBuilder answer = new StringBuilder();
+    	
+    	for(int i = 0 ; i<arr.length ; i++) {
+    		
+    		StringBuilder temp = new StringBuilder(arr[i]);
+    		
+    		if(i!=0)
+    			answer.append(' ');
+    		
+    		answer.append(temp.reverse().toString());
+    	}
+    	
+    	return answer.toString();
+        
+    }
+}
