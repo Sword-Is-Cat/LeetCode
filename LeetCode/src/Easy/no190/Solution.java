10c727c77f18a027919cf14d5a4ded8df2e83504
jang <jhbsp@naver.com>
2020-08-13 19:48:01 +0900

    solve

---
+++ LeetCode/src/Easy/no190/Solution.java
@@ -0,0 +1,16 @@
+package Easy.no190;
+
+public class Solution {
+	public int reverseBits(int n) {
+
+		int m = 0;
+
+		for (int i = 0; i < 32; i++) {
+			m = (m << 1) | (n & 1);
+			n >>= 1;
+		}
+
+		return m;
+
+	}
+}
