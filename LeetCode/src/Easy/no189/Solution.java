c598fa00c8acd2267aa1de3d79db926dc555ffcf
jang <jhbsp@naver.com>
2020-08-13 19:33:17 +0900

    solve

---
+++ LeetCode/src/Easy/no189/Solution.java
@@ -0,0 +1,23 @@
+package Easy.no189;
+
+class Solution {
+	public void rotate(int[] nums, int k) {
+		
+		k %= nums.length;
+
+		int[] arr = new int[k];
+
+		for (int i = 1; i <= k; i++) {
+			arr[k - i] = nums[nums.length - i];
+		}
+
+		for (int i = 1; i <= nums.length - k; i++) {
+			nums[nums.length - i] = nums[nums.length - i - k];
+		}
+
+		for (int i = 0; i < k; i++) {
+			nums[i] = arr[i];
+		}
+
+	}
+}
