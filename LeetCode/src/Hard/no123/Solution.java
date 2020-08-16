c6566ca1a1d9a4d9bb526c44e0c88ffa27ad795e
jang <jhbsp@naver.com>
2020-08-16 17:13:37 +0900

    solve

---
+++ LeetCode/src/Hard/no123/Solution.java
@@ -0,0 +1,25 @@
+package Hard.no123;
+
+class Solution {
+
+	public int maxProfit(int[] prices) {
+
+		if (prices.length == 0)
+			return 0;
+
+		int firstBuy = Integer.MIN_VALUE;
+		int firstSell = 0;
+		int secondBuy = Integer.MIN_VALUE;
+		int secondSell = 0;
+
+		for (int i : prices) {
+
+			firstBuy = Math.max(firstBuy, -i);
+			firstSell = Math.max(firstSell, firstBuy + i);
+			secondBuy = Math.max(secondBuy, firstSell - i);
+			secondSell = Math.max(secondSell, secondBuy + i);
+		}
+
+		return secondSell;
+	}
+}
