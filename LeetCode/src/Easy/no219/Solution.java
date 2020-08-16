1f057cd1f1582ad86f22a836dca869aa98c7fd12
jang <jhbsp@naver.com>
2020-08-16 17:21:41 +0900

    solve

---
+++ LeetCode/src/Easy/no219/Solution.java
@@ -0,0 +1,21 @@
+package Easy.no219;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k)
+				return true;
+			else
+				map.put(nums[i], i);
+		}
+
+		return false;
+
+	}
+}
