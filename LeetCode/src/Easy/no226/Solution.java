2ab2b953e91336c631715ed2cb2a009f82c3f719
jang <jhbsp@naver.com>
2020-08-16 17:30:09 +0900

    align

--- LeetCode/src/Easy/no226/Solution.java
+++ LeetCode/src/Easy/no226/Solution.java
@@ -2,15 +2,15 @@ package Easy.no226;
 
 class Solution {
 	public TreeNode invertTree(TreeNode root) {
-		
-		if(root==null)
+
+		if (root == null)
 			return root;
-		
+
 		TreeNode temp = root.left;
-		
+
 		root.left = invertTree(root.right);
 		root.right = invertTree(temp);
-		
+
 		return root;
 	}
 }
