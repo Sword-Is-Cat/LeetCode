349d7f3e954b01c857efa32a5391db7c93ad34b3
jang <jhbsp@naver.com>
2020-08-19 15:57:29 +0900

    solve

---
+++ LeetCode/src/Easy/no404/Solution.java
@@ -0,0 +1,35 @@
+package Easy.no404;
+
+class Solution {
+	public int sumOfLeftLeaves(TreeNode root) {
+		
+		if (root == null)
+			return 0;
+		
+		int sum = 0;
+		
+		if (root.left != null && root.left.left == null && root.left.right == null)
+			sum += root.left.val;
+		
+		return sum + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
