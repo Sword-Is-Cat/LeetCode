cc26632099046415536efc15217b5bec6d6640ab
jang <jhbsp@naver.com>
2020-08-19 14:33:58 +0900

    solve

---
+++ LeetCode/src/Easy/no237/Solution.java
@@ -0,0 +1,18 @@
+package Easy.no237;
+
+class Solution {
+	public void deleteNode(ListNode node) {
+
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
