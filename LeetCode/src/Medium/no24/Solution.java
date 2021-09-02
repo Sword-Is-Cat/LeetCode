96808e9f38a806d0d3671dc3dae9854da4201935
jang <jhbsp@naver.com>
2021-09-03 01:10:54 +0900

    210903

---
+++ LeetCode/src/Medium/no24/Solution.java
@@ -0,0 +1,41 @@
+package Medium.no24;
+
+class Solution {
+	public ListNode swapPairs(ListNode head) {
+
+		ListNode root = new ListNode(0, head);
+		ListNode node = root;
+
+		while (node.next != null && node.next.next != null) {
+			
+			ListNode after = node.next.next.next;
+			ListNode temp = node.next.next;
+			
+			temp.next = node.next;
+			temp.next.next = after;
+			node.next = temp;
+			
+			node = node.next.next;
+			
+		}
+
+		return root.next;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
