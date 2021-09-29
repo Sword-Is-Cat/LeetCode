8bc20de888f1ca8d9582d253306e356aaa3e0085
jang <jhbsp@naver.com>
2021-09-30 02:36:12 +0900

    210930

---
+++ LeetCode/src/Easy/no929/Solution.java
@@ -0,0 +1,24 @@
+package Easy.no929;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+    	
+    	Set<String> set = new HashSet<>();
+    	
+    	for(String email : emails) {
+    		
+    		String[] mail = email.split("@");
+    		
+    		String id = mail[0];
+    		
+    		set.add(mail[0].split("\\+")[0].replace(".", "").concat("@").concat(mail[1]));
+    		
+    	}
+    	
+    	return set.size();
+        
+    }
+}
