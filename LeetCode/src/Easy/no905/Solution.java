a863387b6a69149ce8a12e4aefa0d0c12df47dd4
jang <jhbsp@naver.com>
2021-10-21 19:20:52 +0900

    211021

---
+++ LeetCode/src/Easy/no905/Solution.java
@@ -0,0 +1,21 @@
+package Easy.no905;
+
+class Solution {
+	public int[] sortArrayByParity(int[] nums) {
+
+		int[] arr = new int[nums.length];
+
+		int even = 0;
+		int odd = arr.length - 1;
+
+		for (int no : nums) {
+			if (no % 2 == 0)
+				arr[even++] = no;
+			else
+				arr[odd--] = no;
+		}
+		
+		return arr;
+
+	}
+}
