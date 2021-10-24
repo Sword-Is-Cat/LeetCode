78eeba2979ba10bec6dae3041bd6669ccb9f1ffc
jang <jhbsp@naver.com>
2021-10-24 18:10:34 +0900

    211024

---
+++ LeetCode/src/Easy/no1051/Solution.java
@@ -0,0 +1,19 @@
+package Easy.no1051;
+
+import java.util.Arrays;
+
+class Solution {
+	public int heightChecker(int[] heights) {
+
+		int[] arr = heights.clone();
+		Arrays.sort(arr);
+
+		int cnt = 0;
+
+		for (int i = 0; i < arr.length; i++)
+			if (arr[i] != heights[i]) cnt++;
+		
+		return cnt;
+
+	}
+}
