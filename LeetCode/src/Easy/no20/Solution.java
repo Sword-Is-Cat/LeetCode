f02e2108e3476ff296c6951d7d129dfddbc3d371
jang <jhbsp@naver.com>
2022-03-16 09:32:47 +0900

    220316

--- LeetCode/src/Easy/no20/Solution.java
+++ LeetCode/src/Easy/no20/Solution.java
@@ -3,41 +3,31 @@ package Easy.no20;
 import java.util.Stack;
 
 class Solution {
+
+	final char[][] pairs = { { '(', ')' }, { '[', ']' }, { '{', '}' } };
+
 	public boolean isValid(String s) {
 
 		Stack<Character> stack = new Stack<>();
-		char[] arr = s.toCharArray();
 
-		for (char ch : arr) {
-			if (stack.empty())
+		for (char ch : s.toCharArray()) {
+			if (stack.isEmpty()) {
 				stack.add(ch);
-			else
-				switch (ch) {
-				case ')':
-					if (stack.peek() == '(')
-						stack.pop();
-					else
-						stack.add(ch);
-					break;
-				case '}':
-					if (stack.peek() == '{')
-						stack.pop();
-					else
-						stack.add(ch);
-					break;
-				case ']':
-					if (stack.peek() == '[')
-						stack.pop();
-					else
-						stack.add(ch);
-					break;
-				default:
-					stack.add(ch);
-				}
+			} else {
 
+				boolean isPopped = false;
+				for (int i = 0; i < 3; i++) {
+					char[] pair = pairs[i];
+					if (stack.peek() == pair[0] && ch == pair[1]) {
+						stack.pop();
+						isPopped = true;
+						break;
+					}
+				}
+				if (!isPopped) stack.add(ch);
+			}
 		}
 
-		return (stack.size() == 0) ? true : false;
-
+		return stack.isEmpty();
 	}
 }
---
+++ LeetCode/src/Easy/no20/Solution_.java
@@ -0,0 +1,43 @@
+package Easy.no20;
+
+import java.util.Stack;
+
+class Solution_ {
+	public boolean isValid(String s) {
+
+		Stack<Character> stack = new Stack<>();
+		char[] arr = s.toCharArray();
+
+		for (char ch : arr) {
+			if (stack.empty())
+				stack.add(ch);
+			else
+				switch (ch) {
+				case ')':
+					if (stack.peek() == '(')
+						stack.pop();
+					else
+						stack.add(ch);
+					break;
+				case '}':
+					if (stack.peek() == '{')
+						stack.pop();
+					else
+						stack.add(ch);
+					break;
+				case ']':
+					if (stack.peek() == '[')
+						stack.pop();
+					else
+						stack.add(ch);
+					break;
+				default:
+					stack.add(ch);
+				}
+
+		}
+
+		return (stack.size() == 0) ? true : false;
+
+	}
+}
