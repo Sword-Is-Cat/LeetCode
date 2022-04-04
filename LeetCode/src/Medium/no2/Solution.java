45ecdba6d03c0babb229da3d27e8ef646ce581e6
jang <jhbsp@naver.com>
2022-04-04 12:41:45 +0900

    220404

--- LeetCode/src/Medium/no2/Solution.java
+++ LeetCode/src/Medium/no2/Solution.java
@@ -6,38 +6,38 @@ class Solution {
 		ListNode answer = new ListNode();
 		ListNode temp = answer;
 		boolean isCarry = false;
-		
-		while(l1!=null || l2!=null) {
-			
+
+		while (l1 != null || l2 != null) {
+
 			temp.next = new ListNode();
 			temp = temp.next;
-			
-			if(l1!=null) {
+
+			if (l1 != null) {
 				temp.val += l1.val;
 				l1 = l1.next;
 			}
-			
-			if(l2!=null) {
+
+			if (l2 != null) {
 				temp.val += l2.val;
 				l2 = l2.next;
 			}
-			
-			if(isCarry) {
+
+			if (isCarry) {
 				temp.val += 1;
 				isCarry = false;
 			}
-			
-			if(temp.val>9) {
-				temp.val -=10;
+
+			if (temp.val > 9) {
+				temp.val -= 10;
 				isCarry = true;
 			}
-						
+
 		}
-		
-		if(isCarry) {
+
+		if (isCarry) {
 			temp.next = new ListNode(1);
 		}
-				
+
 		return answer.next;
 	}
 }
@@ -46,7 +46,8 @@ class ListNode {
 	int val;
 	ListNode next;
 
-	ListNode() {}
+	ListNode() {
+	}
 
 	ListNode(int val) {
 		this.val = val;
