89aee0e85872ad20d10bda2046752b6a9d33ce8a
jang <jhbsp@naver.com>
2022-04-14 22:35:01 +0900

    220414

--- LeetCode/src/Easy/no700/Solution.java
+++ LeetCode/src/Easy/no700/Solution.java
@@ -2,18 +2,11 @@ package Easy.no700;
 
 class Solution {
 	public TreeNode searchBST(TreeNode root, int val) {
-		
-		if(root == null)
+
+		if (root == null || root.val == val)
 			return root;
-		
-		if(root.val == val)
-			return root;
-		else if(root.val > val)
-			return searchBST(root.left, val);
-		else if(root.val < val)
-			return searchBST(root.right, val);
 		else
-			return null;
+			return searchBST(root.val > val ? root.left : root.right, val);
 
 	}
 }
