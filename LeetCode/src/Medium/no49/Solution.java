1aec9e546c60886cb100812771238637d2559e1f
jang <jhbsp@naver.com>
2022-04-26 10:26:00 +0900

    220426

---
+++ LeetCode/src/Medium/no49/Solution.java
@@ -0,0 +1,36 @@
+package Medium.no49;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+	public List<List<String>> groupAnagrams(String[] strs) {
+
+		Map<String, List<String>> map = new HashMap<>();
+
+		for (String str : strs) {
+
+			String key = convertStr(str);
+			if (!map.containsKey(key))
+				map.put(key, new ArrayList<>());
+			map.get(key).add(str);
+
+		}
+
+		List<List<String>> list = new ArrayList<>();
+
+		for (String key : map.keySet())
+			list.add(map.get(key));
+
+		return list;
+	}
+
+	String convertStr(String str) {
+		char[] arr = str.toCharArray();
+		Arrays.sort(arr);
+		return new String(arr);
+	}
+}
