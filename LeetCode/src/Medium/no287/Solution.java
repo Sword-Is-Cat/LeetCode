a2bbbce2e9b9ffebfdf5ae926cbfc29cc6b3383a
jang <jhbsp@naver.com>
2022-05-09 13:56:41 +0900

    220509

---
+++ LeetCode/src/Medium/no287/Solution.java
@@ -0,0 +1,21 @@
+package Medium.no287;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+	public int findDuplicate(int[] nums) {
+
+		Set<Integer> set = new HashSet<>();
+
+		int rst = -1;
+
+		for (int no : nums) {
+			if (!set.add(no)) {
+				rst = no;
+				break;
+			}
+		}
+		return rst;
+	}
+}
