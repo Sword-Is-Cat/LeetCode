25c3c2b4770f22a339556d081b60304f76e954a2
jang <jhbsp@naver.com>
2022-05-11 09:29:43 +0900

    220511

---
+++ LeetCode/src/Medium/no341/NestedIterator.java
@@ -0,0 +1,39 @@
+package Medium.no341;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class NestedIterator implements Iterator<Integer> {
+
+	List<Integer> list;
+	int idx = -1;
+
+	public NestedIterator(List<NestedInteger> nestedList) {
+
+		list = new ArrayList<>();
+		append(list, nestedList);
+
+	}
+
+	void append(List<Integer> list, List<NestedInteger> nList) {
+
+		for (NestedInteger nInt : nList) {
+
+			if (nInt.isInteger())
+				list.add(nInt.getInteger());
+			else
+				append(list, nInt.getList());
+		}
+	}
+
+	@Override
+	public Integer next() {
+		return list.get(++idx);
+	}
+
+	@Override
+	public boolean hasNext() {
+		return idx + 1 < list.size();
+	}
+}
