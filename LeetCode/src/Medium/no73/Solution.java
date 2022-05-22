06b78e6621407efda1060d90fb5f5b2099c73cdd
jang <jhbsp@naver.com>
2022-05-22 22:49:27 +0900

    220522

---
+++ LeetCode/src/Medium/no73/Solution.java
@@ -0,0 +1,28 @@
+package Medium.no73;
+
+class Solution {
+	public void setZeroes(int[][] matrix) {
+
+		boolean[] rowCheck = new boolean[matrix.length];
+		boolean[] colCheck = new boolean[matrix[0].length];
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+
+				if (matrix[i][j] == 0) {
+					rowCheck[i] = true;
+					colCheck[j] = true;
+				}
+			}
+		}
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+
+				if (rowCheck[i] || colCheck[j]) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+}
