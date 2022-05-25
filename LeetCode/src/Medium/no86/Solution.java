6034214271ac84a372d63a55aca1f54d3b2c9bb3
jang <jhbsp@naver.com>
2022-05-25 09:33:55 +0900

    220525

---
+++ LeetCode/src/Medium/no86/Solution.java
@@ -0,0 +1,35 @@
+package Medium.no86;
+
+import java.util.ArrayList;
+
+class Solution {
+	public ListNode partition(ListNode head, int x) {
+
+		ArrayList<ListNode> ltList = new ArrayList<>();
+		ArrayList<ListNode> gtList = new ArrayList<>();
+
+		while (head != null) {
+			if (head.val < x)
+				ltList.add(head);
+			else
+				gtList.add(head);
+			head = head.next;
+		}
+
+		ListNode root = new ListNode();
+		ListNode temp = root;
+
+		for (ListNode node : ltList) {
+			temp.next = node;
+			temp = temp.next;
+		}
+
+		for (ListNode node : gtList) {
+			temp.next = node;
+			temp = temp.next;
+		}
+
+		temp.next = null;
+		return root.next;
+	}
+}
