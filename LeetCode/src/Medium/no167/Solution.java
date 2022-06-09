92c6447dfcde18cf45c393ab801d5546a1629aed
jang <jhbsp@naver.com>
2022-06-09 09:27:50 +0900

    220609

---
+++ LeetCode/src/Medium/no167/Solution.java
@@ -0,0 +1,21 @@
+package Medium.no167;
+
+class Solution {
+	public int[] twoSum(int[] numbers, int target) {
+
+		int left = 0, right = numbers.length - 1;
+
+		while (left < right) {
+
+			if (numbers[left] + numbers[right] > target)
+				right--;
+			else if (numbers[left] + numbers[right] < target)
+				left++;
+			else
+				break;
+
+		}
+
+		return new int[] { left + 1, right + 1 };
+	}
+}
