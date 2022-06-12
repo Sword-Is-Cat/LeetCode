40f288def143b9887fa57bf7899899a21ec2acb2
jang <jhbsp@naver.com>
2022-06-12 22:15:15 +0900

    220612

---
+++ LeetCode/src/Medium/no347/Solution.java
@@ -0,0 +1,51 @@
+package Medium.no347;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Solution {
+	public int[] topKFrequent(int[] nums, int k) {
+
+		Map<Integer, InstantVO> map = new HashMap<>();
+
+		for (int num : nums) {
+			if (!map.containsKey(num)) {
+				map.put(num, new InstantVO(num));
+			}
+			map.get(num).add();
+		}
+
+		Queue<InstantVO> que = new PriorityQueue<>((a, b) -> b.cnt - a.cnt);
+
+		for (int key : map.keySet())
+			que.add(map.get(key));
+
+		int[] result = new int[k];
+		int idx = 0;
+
+		while (idx < k) {
+			result[idx++] = que.poll().no;
+		}
+
+		return result;
+
+	}
+}
+
+class InstantVO {
+
+	int no;
+	int cnt;
+
+	InstantVO(int no) {
+		this.no = no;
+		this.cnt = 0;
+	}
+
+	void add() {
+		cnt++;
+	}
+
+}
