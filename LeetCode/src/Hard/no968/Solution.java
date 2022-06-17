ab8f25039153678bfc589d7f3bd69d775070d63e
jang <jhbsp@naver.com>
2022-06-17 10:16:29 +0900

    220617

--- LeetCode/src/Hard/no968/Solution.java
+++ LeetCode/src/Hard/no968/Solution.java
@@ -2,39 +2,33 @@ package Hard.no968;
 
 class Solution {
 
-	int cnt = 0;
+	int camCnt = 0;
 
 	public int minCameraCover(TreeNode root) {
 
 		if (process(root) == 0)
-			cnt++;
-		return cnt;
+			camCnt++;
+		return camCnt;
 	}
 
 	/**
-	 * 
 	 * @param node
-	 * @return 0->내PT가 카메라 여야 함, 1->내가 카메라임, 2->내child가 카메라임
+	 * @return 0 -> pt Node need to be CAM, 1 -> this is CAM, 2 -> one of my childNode is CAM
 	 */
 	int process(TreeNode node) {
 
-		int status = 3;
+		int status = 2;
 
-		if (node.left == null && node.right == null) {
-			status = -1;
-		} else if (node.left == null) {
-			status = process(node.right);
-		} else if (node.right == null) {
-			status = process(node.left);
-		} else {
+		if (node != null) {
+
 			status = Math.min(status, process(node.left));
 			status = Math.min(status, process(node.right));
+
+			status = (status + 1) % 3;
+
+			if (status == 1)
+				camCnt++;
 		}
-
-		status = (status + 1) % 3;
-
-		if (status == 1)
-			cnt++;
 
 		return status;
 	}
