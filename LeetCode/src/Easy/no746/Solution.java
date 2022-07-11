35d563cbfbb0bae969e55397f60a79a0d7712b72
jang <jhbsp@naver.com>
2022-07-11 20:16:32 +0900

    220711

--- LeetCode/src/Easy/no746/Solution.java
+++ LeetCode/src/Easy/no746/Solution.java
@@ -8,8 +8,8 @@ class Solution {
 		sum[0] = cost[0];
 		sum[1] = cost[1];
 
-		for (int i = 2; i < sum.length; i++) 
-			sum[i] = Math.min(sum[i - 1], sum[i - 2]) + i == cost.length ? 0 : cost[i];
+		for (int i = 2; i < sum.length; i++)
+			sum[i] = Math.min(sum[i - 1], sum[i - 2]) + (i == cost.length ? 0 : cost[i]);
 
 		return sum[cost.length];
 	}
