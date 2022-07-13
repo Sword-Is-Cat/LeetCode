0f7473239b69157962f161350d4798bee42bb40a
jang <jhbsp@naver.com>
2022-07-13 18:47:22 +0900

    220713

--- LeetCode/src/Medium/no102/Solution.java
+++ LeetCode/src/Medium/no102/Solution.java
@@ -4,39 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-    	
-    	List<List<Integer>> result = new ArrayList<>();
-    	if(root==null) return result;
-    	
-    	List<List<TreeNode>> bigList = new ArrayList<>();
-    	List<TreeNode> list = new ArrayList<>();
-    	List<TreeNode> newList = null;
-    	list.add(root);
-    	
-    	while(list.size()>0) {
-    		
-    		bigList.add(list);
-    		newList = new ArrayList<>();
-    		
-    		for(TreeNode node : list) {
-    			if(node.left!=null)
-    				newList.add(node.left);
-    			if(node.right!=null)
-    				newList.add(node.right);
-    		}
-    		
-    		list = newList;
-    	}
-    	
-    	for(int i = 0 ; i<bigList.size() ; i++) {
-    		
-    		result.add(new ArrayList<>());
-    		
-    		for(TreeNode node : bigList.get(i))
-    			result.get(i).add(node.val);
-    	}
-        
-    	return result;
-    }
+
+	List<List<Integer>> list;
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+
+		list = new ArrayList<>();
+
+		process(root, 0);
+
+		return list;
+	}
+
+	void process(TreeNode node, int depth) {
+
+		if (node == null)
+			return;
+
+		if (list.size() == depth)
+			list.add(new ArrayList<>());
+
+		list.get(depth).add(node.val);
+
+		process(node.left, depth + 1);
+		process(node.right, depth + 1);
+
+	}
 }
