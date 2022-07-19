c3def6770404f9c969e69e1cd5753d419e75ee9b
jang <jhbsp@naver.com>
2022-07-19 11:31:46 +0900

    220719

---
+++ LeetCode/src/Medium/no881/Solution.java
@@ -0,0 +1,25 @@
+package Medium.no881;
+
+import java.util.Arrays;
+
+class Solution {
+	public int numRescueBoats(int[] people, int limit) {
+
+		Arrays.sort(people);
+
+		int left = 0;
+		int right = people.length - 1;
+
+		int cnt = 0;
+
+		while (left <= right) {
+
+			cnt++;
+
+			if (people[left] + people[right--] <= limit) {
+				left++;
+			}
+		}
+		return cnt;
+	}
+}
