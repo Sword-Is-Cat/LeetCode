dea9f0d6933c59391f00576494545e0be85f52bc
jang <jhbsp@naver.com>
2022-08-10 09:27:37 +0900

    220810

--- LeetCode/src/Easy/no108/Solution.java
+++ LeetCode/src/Easy/no108/Solution.java
@@ -3,54 +3,31 @@ package Easy.no108;
 import java.util.Arrays;
 
 class Solution {
+	
 	public TreeNode sortedArrayToBST(int[] nums) {
 
 		TreeNode node = null;
 		int length = nums.length;
 
-		if (length < 4) {
+		if (length > 3) {
 
-			TreeNode node1 = new TreeNode(nums[0]);
+			int idx = length / 2;
+			node = new TreeNode(nums[idx]);
+			node.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, idx));
+			node.right = sortedArrayToBST(Arrays.copyOfRange(nums, idx + 1, length));
 
-			if (length > 1) {
+		} else if (length > 1) {
 
-				TreeNode node2 = new TreeNode(nums[1]);
-				node = node2;
-				node.left = node1;
+			node = new TreeNode(nums[1]);
+			node.left = new TreeNode(nums[0]);
+			node.right = length == 3 ? new TreeNode(nums[2]) : null;
 
-				if (length == 3) {
+		} else if (length > 0) {
 
-					node.right = new TreeNode(nums[2]);
-
-				} else {
-					node.right = null;
-				}
-
-			} else {
-				node = node1;
-				node.left = null;
-				node.right = null;
-			}
-
-		} else {
-
-			node = new TreeNode(nums[length / 2]);
-			node.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, length / 2));
-			node.right = sortedArrayToBST(Arrays.copyOfRange(nums, length / 2 + 1, length));
+			node = new TreeNode(nums[0]);
 
 		}
 
 		return node;
-
-	}
-}
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
 	}
 }
