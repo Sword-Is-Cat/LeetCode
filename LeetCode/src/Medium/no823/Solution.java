9bd495cca08fe713a2935d3af4dda7dc32068856
jang <jhbsp@naver.com>
2022-08-12 08:38:57 +0900

    220812

---
+++ LeetCode/src/Medium/no823/Solution.java
@@ -0,0 +1,38 @@
+package Medium.no823;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+
+	final static int MOD = (int) 1e9 + 7;
+
+	public int numFactoredBinaryTrees(int[] arr) {
+
+		HashMap<Integer, Long> map = new HashMap<>();
+		long ans = 0;
+		Arrays.sort(arr);
+
+		for (int i = 0; i < arr.length; i++) {
+
+			long value = 1;
+			int num = arr[i];
+
+			for (int j = 0; j < i; j++) {
+
+				int div = arr[j];
+
+				if (num % div == 0 && map.containsKey(num / div)) {
+					value += (map.get(div) * map.get(num / div)) % MOD;
+				}
+
+			}
+
+			map.put(num, value % MOD);
+			ans += map.get(num);
+
+		}
+
+		return (int) (ans % MOD);
+	}
+}
