cf361537e77e907db6ac686f7d6e336b9ca378f4
jang <jhbsp@naver.com>
2022-09-02 09:17:18 +0900

    220902

---
+++ LeetCode/src/Medium/no1091/Solution.java
@@ -0,0 +1,54 @@
+package Medium.no1091;
+
+import java.util.HashSet;
+
+class Solution {
+
+	final int[][] DIRECTION = { { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 } };
+
+	public int shortestPathBinaryMatrix(int[][] grid) {
+
+		if (grid[0][0] == 1)
+			return -1;
+
+		int dist = 1;
+		if (grid.length == 1)
+			return dist;
+
+		HashSet<int[]> set = new HashSet<>();
+		set.add(new int[] { 0, 0 });
+
+		HashSet<int[]> newSet;
+
+		while (set.size() > 0) {
+
+			dist++;
+			newSet = new HashSet<>();
+			for (int[] point : set) {
+
+				for (int[] direct : DIRECTION) {
+
+					int row = point[0] + direct[0];
+					int col = point[1] + direct[1];
+
+					if (isValid(row, col, grid)) {
+						
+						if (row + 1 == grid.length && col + 1 == grid.length)
+							return dist;
+						
+						grid[row][col] = 1;
+						newSet.add(new int[] { row, col });
+					}
+				}
+			}
+			set = newSet;
+		}
+
+		return -1;
+	}
+
+	boolean isValid(int row, int col, int[][] grid) {
+
+		return row >= 0 && row < grid.length && col >= 0 && col < grid.length && grid[row][col] == 0;
+	}
+}
