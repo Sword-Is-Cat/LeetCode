84f6a3ae65caf8d9e38f6e20f27707c9f107410f
jang <jhbsp@naver.com>
2022-09-30 11:25:49 +0900

    220930

---
+++ LeetCode/src/Medium/no229/Solution.java
@@ -0,0 +1,26 @@
+package Medium.no229;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+	public List<Integer> majorityElement(int[] nums) {
+
+		HashSet<Integer> set = new HashSet<>();
+		int size = nums.length / 3;
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i + size < nums.length; i++) {
+
+			if (nums[i] == nums[i + size])
+				set.add(nums[i]);
+
+		}
+
+		return set.stream().collect(Collectors.toList());
+
+	}
+}
