4803c94f90918aec44ac1b324d74e25d0d6c0c72
jang <jhbsp@naver.com>
2022-10-04 10:21:40 +0900

    221004

---
+++ LeetCode/src/Medium/no1155/Solution.java
@@ -0,0 +1,15 @@
+package Medium.no1155;
+
+import java.util.HashMap;
+
+class Solution {
+	public int numRollsToTarget(int n, int k, int target) {
+
+		HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
+		map.put(0, new HashMap<>());
+		
+		
+		return 0;
+
+	}
+}
