762049c6ca4bfb136c52787be702d97b3a94365e
jang <jhbsp@naver.com>
2022-10-05 14:12:10 +0900

    221005

--- LeetCode/src/Medium/no1155/Solution.java
+++ LeetCode/src/Medium/no1155/Solution.java
@@ -3,13 +3,30 @@ package Medium.no1155;
 import java.util.HashMap;
 
 class Solution {
+
+	HashMap<String, Integer> map = new HashMap<>();
+	final int MOD = 1000000007;
+
 	public int numRollsToTarget(int n, int k, int target) {
 
-		HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
-		map.put(0, new HashMap<>());
-		
-		
-		return 0;
+		if (n == 0)
+			return target == 0 ? 1 : 0;
+
+		String key = n + "/" + k + "/" + target;
+
+		if (!map.containsKey(key)) {
+
+			long rst = 0;
+
+			for (int val = 1; val <= k; val++) {
+				rst += numRollsToTarget(n - 1, k, target - val);
+			}
+
+			map.put(key, (int) (rst % MOD));
+
+		}
+
+		return map.get(key);
 
 	}
 }
