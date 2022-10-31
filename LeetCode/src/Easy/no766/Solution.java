014cec57c2445eb3e2a074c65fa330b88ab762f6
jang <jhbsp@naver.com>
2022-10-31 09:33:21 +0900

    221031

---
+++ LeetCode/src/Easy/no766/Solution.java
@@ -0,0 +1,15 @@
+package Easy.no766;
+
+class Solution {
+	public boolean isToeplitzMatrix(int[][] matrix) {
+
+		for (int row = 0; row < matrix.length - 1; row++) {
+			for (int col = 0; col < matrix[row].length - 1; col++) {
+				if (matrix[row][col] != matrix[row + 1][col + 1])
+					return false;
+			}
+		}
+
+		return true;
+	}
+}
