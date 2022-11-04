7b15ad9e3ab61eecfa0e46df932ec3a97a988102
jang <jhbsp@naver.com>
2022-11-04 10:23:53 +0900

    221104

--- LeetCode/src/Easy/no345/Solution.java
+++ LeetCode/src/Easy/no345/Solution.java
@@ -5,33 +5,31 @@ class Solution {
 
 		char[] arr = s.toCharArray();
 
-		int i = 0;
-		int j = arr.length - 1;
+		int left = 0, right = arr.length - 1;
 
-		while (i < j) {
-			if (checkVowel(arr[i]) && checkVowel(arr[j])) {
-				char temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				i++;
-				j--;
-			} else {
-				while (i < arr.length && !checkVowel(arr[i]))
-					i++;
-				while (j >= 0 && !checkVowel(arr[j]))
-					j--;
+		while (left < right) {
+
+			while (left < right && !isVowel(arr[left]))
+				left++;
+
+			while (left < right && !isVowel(arr[right]))
+				right--;
+
+			if (left < right) {
+				char temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+				left++;
+				right--;
 			}
+
 		}
 
 		return new String(arr);
-
 	}
 
-	public boolean checkVowel(char ch) {
-		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
-				|| ch == 'O' || ch == 'U')
-			return true;
-		else
-			return false;
+	boolean isVowel(char ch) {
+		ch = Character.toLowerCase(ch);
+		return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
 	}
 }
