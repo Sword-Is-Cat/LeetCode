7ea25ea2993a1105a7fafefba22a3acd0054de06
jang <jhbsp@naver.com>
2022-11-11 09:22:37 +0900

    221111

---
+++ LeetCode/src/Medium/no1457/Solution.java
@@ -0,0 +1,36 @@
+package Medium.no1457;
+
+class Solution {
+	public int pseudoPalindromicPaths(TreeNode root) {
+		return dfs(root, new int[10]);
+	}
+
+	int dfs(TreeNode node, int[] counter) {
+
+		int rst = 0;
+
+		if (node != null) {
+
+			counter[node.val]++;
+			
+			if (node.left == null && node.right == null) {
+				
+				int cntOdd = 0;
+				for (int count : counter) {
+					if (count % 2 == 1)
+						cntOdd++;
+				}
+				rst = cntOdd < 2 ? 1 : 0;
+				
+			} else {
+				
+				rst += dfs(node.left, counter);
+				rst += dfs(node.right, counter);
+			}
+			
+			counter[node.val]--;
+		}
+
+		return rst;
+	}
+}
