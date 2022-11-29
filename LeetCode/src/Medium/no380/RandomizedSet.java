5ca86a9bc235860d2c7060c5b7f390f21e7b04a4
jang <jhbsp@naver.com>
2022-11-29 09:38:34 +0900

    221129

---
+++ LeetCode/src/Medium/no380/RandomizedSet.java
@@ -0,0 +1,34 @@
+package Medium.no380;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class RandomizedSet {
+
+	HashSet<Integer> set;
+	ArrayList<Integer> list;
+
+	public RandomizedSet() {
+		set = new HashSet<>();
+	}
+
+	public boolean insert(int val) {
+		if (list != null)
+			list = null;
+		return set.add(val);
+	}
+
+	public boolean remove(int val) {
+		if (list != null)
+			list = null;
+		return set.remove(val);
+	}
+
+	public int getRandom() {
+		if (list == null)
+			list = new ArrayList<>(set);
+
+		return list.get((int) (Math.random() * set.size()));
+
+	}
+}
