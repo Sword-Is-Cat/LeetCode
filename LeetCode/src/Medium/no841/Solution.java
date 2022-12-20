90a8505139b9c50d5df8765ab6493452eac34bab
jang <jhbsp@naver.com>
2022-12-20 10:21:47 +0900

    221220

--- LeetCode/src/Medium/no841/Solution.java
+++ LeetCode/src/Medium/no841/Solution.java
@@ -5,25 +5,25 @@ import java.util.List;
 class Solution {
 
 	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-
-		boolean[] haveKeys = new boolean[rooms.size()];
-		process(rooms, haveKeys, 0);
-
+		// 키 획득여부를 담을 boolean배열 선언
+		boolean[] keys = new boolean[rooms.size()];
+		// 0번방 키는 기본 획득
+		addKey(rooms, keys, 0);
+		// 모든 방의 키 획득했는지 확인하여 return
 		boolean result = true;
-
-		for (int idx = 0; result && idx < haveKeys.length; idx++) {
-			result &= haveKeys[idx];
+		for (int idx = 0; result && idx < keys.length; idx++) {
+			result &= keys[idx];
 		}
-
 		return result;
 	}
 
-	void process(List<List<Integer>> rooms, boolean[] haveKeys, int keyNo) {
-
-		if (!haveKeys[keyNo]) {
-			haveKeys[keyNo] = true;
-			for (int key : rooms.get(keyNo)) {
-				process(rooms, haveKeys, key);
+	// 키 획득 시 해당 방에서 획득하는 다른 키에 대해 재귀호출
+	void addKey(List<List<Integer>> rooms, boolean[] keys, int keyNo) {
+		if (!keys[keyNo]) {
+			// 이미 획득한 키에 대해서는 로직 진행하지 않음
+			keys[keyNo] = true;
+			for (int newKey : rooms.get(keyNo)) {
+				addKey(rooms, keys, newKey);
 			}
 		}
 	}
