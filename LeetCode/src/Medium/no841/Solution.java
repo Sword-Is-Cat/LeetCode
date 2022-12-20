19f9d0e4e416c65868362265529d81c3c1c3685e
jang <jhbsp@naver.com>
2022-12-20 10:23:14 +0900

    221220

--- LeetCode/src/Medium/no841/Solution.java
+++ LeetCode/src/Medium/no841/Solution.java
@@ -19,12 +19,12 @@ class Solution {
 
 	// 키 획득 시 해당 방에서 획득하는 다른 키에 대해 재귀호출
 	void addKey(List<List<Integer>> rooms, boolean[] keys, int keyNo) {
-		if (!keys[keyNo]) {
-			// 이미 획득한 키에 대해서는 로직 진행하지 않음
-			keys[keyNo] = true;
-			for (int newKey : rooms.get(keyNo)) {
-				addKey(rooms, keys, newKey);
-			}
+		// 이미 획득한 키에 대해서는 로직 진행하지 않음
+		if (keys[keyNo])
+			return;
+		keys[keyNo] = true;
+		for (int newKey : rooms.get(keyNo)) {
+			addKey(rooms, keys, newKey);
 		}
 	}
 }
