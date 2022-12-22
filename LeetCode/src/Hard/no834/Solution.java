2b296917be54c31eda808cf6a9997f59372aeecb
jang <jhbsp@naver.com>
2022-12-22 11:32:40 +0900

    221222

--- LeetCode/src/Hard/no834/Solution.java
+++ LeetCode/src/Hard/no834/Solution.java
@@ -1,6 +1,7 @@
 package Hard.no834;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 class Solution {
@@ -8,29 +9,27 @@ class Solution {
 	public int[] sumOfDistancesInTree(int n, int[][] edges) {
 
 		Node[] nodes = new Node[n];
-		for (int i = 0; i < n; i++) {
-			nodes[i] = new Node(i);
-		}
+
+		while (--n >= 0)
+			nodes[n] = new Node(n);
 
 		for (int[] edge : edges) {
 			nodes[edge[0]].addConn(nodes[edge[1]]);
 		}
 
-		int[] ans = new int[n];
+		return Arrays.stream(nodes).mapToInt(node -> node == null ? 0 : getInfo(node, null)[1]).toArray();
 
-		for (int i = 0; i < n; i++)
-			ans[i] = getInfo(nodes[i], null)[1];
-
-		return ans;
 	}
 
 	int[] getInfo(Node node, Node caller) {
 
-		if (caller != null && node.cache.containsKey(caller))
-			return node.cache.get(caller);
+		// 기존에 계산되어 캐싱된 결과가 있다면 그대로 return
+		if (caller != null && node.cache.containsKey(caller.index))
+			return node.cache.get(caller.index);
 
 		int cntDesc = 0, sumDist = 0;
 
+		// 연결된 하위노드(후손포함) 및 그 거리의 합을 계산
 		for (Node child : node.conn) {
 			if (child != caller) {
 				int[] info = getInfo(child, node);
@@ -39,21 +38,26 @@ class Solution {
 			}
 		}
 
+		// 계산결과 캐싱
 		int[] result = new int[] { cntDesc, sumDist };
 		if (caller != null)
-			node.cache.put(caller, result);
-
+			node.cache.put(caller.index, result);
 		return result;
 
 	}
 
 }
 
+/**
+ * 각 노드간의 연결상태 및 연결노드수,거리합을 저장하는 VO
+ * 
+ * @author me
+ */
 class Node {
 
 	final int index;
 	ArrayList<Node> conn;
-	HashMap<Node, int[]> cache;
+	HashMap<Integer, int[]> cache;
 
 	Node(int i) {
 		this.index = i;
