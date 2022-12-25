d4a52febd14f342ced569ac6ec1f2894712cc589
jang <jhbsp@naver.com>
2022-12-25 12:47:13 +0900

    221225

---
+++ LeetCode/src/Easy/no2389/Solution.java
@@ -0,0 +1,37 @@
+package Easy.no2389;
+
+import java.util.Arrays;
+
+class Solution {
+	public int[] answerQueries(int[] nums, int[] queries) {
+
+		Arrays.sort(nums);
+		for (int i = 1; i < nums.length; i++) {
+			nums[i] += nums[i - 1];
+		}
+
+		System.out.println(Arrays.toString(nums));
+
+		for (int i = 0; i < queries.length; i++) {
+			queries[i] = binarySearch(nums, queries[i]);
+		}
+		System.out.println(Arrays.toString(queries));
+
+		return queries;
+	}
+
+	int binarySearch(int[] nums, int value) {
+
+		int left = -1, right = nums.length;
+
+		while (right - left > 1) {
+			int mid = (left + right) / 2;
+			if (value < nums[mid]) {
+				right = mid;
+			} else {
+				left = mid;
+			}
+		}
+		return right;
+	}
+}
