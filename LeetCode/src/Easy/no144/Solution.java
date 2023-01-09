0447a7019a5cf17bcec96c58fee80c7a31ce9869
jang <jhbsp@naver.com>
2023-01-09 09:22:28 +0900

    230109

--- LeetCode/src/Easy/no144/Solution.java
+++ LeetCode/src/Easy/no144/Solution.java
@@ -4,46 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
+
 	public List<Integer> preorderTraversal(TreeNode root) {
-
-		List<Integer> list = new ArrayList<>();
-
-		if (root != null) {
-			addValToList(list, root);
-		}
+		ArrayList<Integer> list = new ArrayList<>();
+		query(list, root);
 		return list;
-
 	}
 
-	public void addValToList(List<Integer> list, TreeNode node) {
-
-		list.add(node.val);
-
-		if (node.left != null)
-			addValToList(list, node.left);
-
-		if (node.right != null)
-			addValToList(list, node.right);
-
-	}
-
-}
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode() {
-	}
-
-	TreeNode(int val) {
-		this.val = val;
-	}
-
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
+	void query(ArrayList<Integer> list, TreeNode node) {
+		if (node != null) {
+			list.add(node.val);
+			query(list, node.left);
+			query(list, node.right);
+		}
 	}
 }
