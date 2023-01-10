2b29523af11b5310cd8fce38a777f1919e79e04c
jang <jhbsp@naver.com>
2023-01-10 09:32:37 +0900

    230110

--- LeetCode/src/Easy/no100/Solution.java
+++ LeetCode/src/Easy/no100/Solution.java
@@ -1,28 +1,18 @@
 package Easy.no100;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
-//class Solution {
-//	public boolean isSameTree(TreeNode p, TreeNode q) {
-//
-//		if (p == null && q == null)
-//			return true;
-//
-//		if (p == null ^ q == null)
-//			return false;
-//
-//		if (p.val != q.val)
-//			return false;
-//
-//		return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
-//
-//	}
-//}
+class Solution {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		// [1]둘다 null인 경우 true
+		if (p == null && q == null)
+			return true;
+		// [2] 한쪽만 null인 경우 false
+		if (p == null ^ q == null)
+			return false;
+		// [3] 둘다 null 이 아닌경우
+		// [3-1] value가 다른경우 false
+		if (p.val != q.val)
+			return false;
+		// [3-2] 두 노드의 value가 같다면, left노드 및 right노드의 일치여부 반환
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+}
---
+++ LeetCode/src/Easy/no100/TreeNode.java
@@ -0,0 +1,8 @@
+package Easy.no100;
+
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
