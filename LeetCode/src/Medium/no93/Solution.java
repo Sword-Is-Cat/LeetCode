ffa9b619e0762eff32c22e19e859738dbbdf8a1f
jang <jhbsp@naver.com>
2023-01-21 15:25:22 +0900

    230121

--- LeetCode/src/Medium/no93/Solution.java
+++ LeetCode/src/Medium/no93/Solution.java
@@ -7,24 +7,21 @@ import java.util.List;
 class Solution {
 
 	HashMap<String, Boolean> cache = new HashMap<>();
+	int length;
 
 	public List<String> restoreIpAddresses(String s) {
 
 		List<String> ans = new ArrayList<>();
-		int length = s.length();
+		length = s.length();
 
 		for (int l1 = 1; l1 < 4; l1++) {
 			for (int l2 = l1 + 1; l2 < l1 + 4; l2++) {
 				for (int l3 = l2 + 1; l3 < l2 + 4; l3++) {
 					for (int l4 = l3 + 1; l4 < l3 + 4; l4++) {
 
-						if (l4 == length) {
-							if (isValid(s.substring(0, l1)) && isValid(s.substring(l1, l2))
-									&& isValid(s.substring(l2, l3)) && isValid(s.substring(l3, l4))) {
-								ans.add(s.substring(0, l1) + "." + s.substring(l1, l2) + "." + s.substring(l2, l3) + "."
-										+ s.substring(l3, l4));
-							}
-						}
+						String str = query(l1, l2, l3, l4, s);
+						if (str != null)
+							ans.add(str);
 					}
 				}
 			}
@@ -33,27 +30,26 @@ class Solution {
 		return ans;
 	}
 
-	boolean isValid(String str) {
+	protected String query(int l1, int l2, int l3, int l4, String s) {
 
-		if (!cache.containsKey(str)) {
+		if (l4 != length)
+			return null;
 
-			boolean result = true;
-			int num = 0;
-
-			if (str.isEmpty() || str.length() > 3)
-				result = false;
-
-			if (result) {
-
-				num = Integer.parseInt(str);
-
-				result &= num <= 255;
-				result &= str.equals(Integer.toString(num));
-			}
-
-			cache.put(str, result);
+		String s1 = s.substring(0, l1), s2 = s.substring(l1, l2), s3 = s.substring(l2, l3), s4 = s.substring(l3);
+		if (isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)) {
+			return new StringBuilder().append(s1).append('.').append(s2).append('.').append(s3).append('.').append(s4)
+					.toString();
 		}
 
+		return null;
+
+	}
+
+	protected Boolean isValid(String str) {
+		if (!cache.containsKey(str)) {
+			int no = Integer.parseInt(str);
+			cache.put(str, str.equals(Integer.toString(no)) && no < 256);
+		}
 		return cache.get(str);
 	}
 }
