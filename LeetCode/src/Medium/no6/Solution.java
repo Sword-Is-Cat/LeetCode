a196947f73c8e7a2a62aa93bf2b417112ad06983
jang <jhbsp@naver.com>
2023-02-03 12:59:40 +0900

    230203

--- LeetCode/src/Medium/no6/Solution.java
+++ LeetCode/src/Medium/no6/Solution.java
@@ -7,28 +7,26 @@ class Solution {
 			return s;
 
 		StringBuilder[] sbArr = new StringBuilder[numRows];
-
-		for (int i = 0; i < sbArr.length; i++) {
+		for (int i = 0; i < numRows; i++)
 			sbArr[i] = new StringBuilder();
+
+		for (int idx = 0; idx < s.length(); idx++) {
+
+			char ch = s.charAt(idx);
+			int position = idx % (numRows * 2 - 2);
+			if (position >= numRows)
+				position = 2 * numRows - 2 - position;
+
+			sbArr[position].append(ch);
+
 		}
 
-		char[] chArr = s.toCharArray();
-
-		for (int i = 0; i < chArr.length; i++) {
-			int mod = i % (numRows * 2 - 2);
-			if (mod < numRows - 1)
-				sbArr[mod].append(chArr[i]);
-			else
-				sbArr[numRows * 2 - 2 - mod].append(chArr[i]);
-		}
-
-		StringBuilder answer = new StringBuilder();
+		StringBuilder ans = new StringBuilder();
 
 		for (int i = 0; i < sbArr.length; i++) {
-			answer.append(sbArr[i]);
+			ans.append(sbArr[i]);
 		}
 
-		return answer.toString();
-
+		return ans.toString();
 	}
 }
