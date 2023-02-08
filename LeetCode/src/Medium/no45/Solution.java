c535c7475be5d36193b12e78b27764bf616a3f73
jang <jhbsp@naver.com>
2023-02-08 13:08:57 +0900

    230208

--- LeetCode/src/Medium/no45/Solution.java
+++ LeetCode/src/Medium/no45/Solution.java
@@ -1,33 +1,19 @@
 package Medium.no45;
 
 class Solution {
+	
 	public int jump(int[] nums) {
-
-		if (nums.length == 1)
-			return 0;
-
-		int cnt = 0;
-		int expect = nums[0];
-		int temp = expect;
-
-		for (int i = 0; i < nums.length; i++) {
-
-			if (expect >= nums.length - 1) {
-				cnt++;
-				break;
+		
+		int jump = 0, next = 0, temp = 0;
+		
+		for (int point = 0; point < nums.length; point++) {
+			
+			if (next < point) {
+				next = temp;
+				jump++;
 			}
-
-			nums[i] += i;
-
-			temp = Math.max(temp, nums[i]);
-			if (i == expect) {
-				cnt++;
-				expect = temp;
-
-			}
-
+			temp = Math.max(temp, nums[point] + point);
 		}
-
-		return cnt;
+		return jump;
 	}
 }
