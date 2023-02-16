ca6f17ea3fd6bf90a42faeb9dbdea6a60cddd123
jang <jhbsp@naver.com>
2023-02-16 09:16:27 +0900

    230216

--- LeetCode/src/Easy/no104/Solution.java
+++ LeetCode/src/Easy/no104/Solution.java
@@ -1,36 +1,7 @@
 package Easy.no104;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
-//class Solution {
-//
-//	int answer;
-//
-//	public int maxDepth(TreeNode root) {
-//
-//		maxDepth(root, 0);
-//
-//		return answer;
-//
-//	}
-//
-//	public void maxDepth(TreeNode root, int depth) {
-//
-//		if (root == null)
-//			answer = (answer > depth) ? answer : depth;
-//
-//		else {
-//			maxDepth(root.left, depth + 1);
-//			maxDepth(root.right, depth + 1);
-//		}
-//
-//	}
-//}
+class Solution {
+	public int maxDepth(TreeNode root) {
+		return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+}
