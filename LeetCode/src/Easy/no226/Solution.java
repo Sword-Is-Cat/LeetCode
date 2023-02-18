126f517409a2b6b0f1cb6d49993e3fed952c7657
jang <jhbsp@naver.com>
2023-02-18 18:52:29 +0900

    230218

--- LeetCode/src/Easy/no226/Solution.java
+++ LeetCode/src/Easy/no226/Solution.java
@@ -2,34 +2,11 @@ package Easy.no226;
 
 class Solution {
 	public TreeNode invertTree(TreeNode root) {
-
-		if (root == null)
-			return root;
-
-		TreeNode temp = root.left;
-
-		root.left = invertTree(root.right);
-		root.right = invertTree(temp);
-
+		if (root != null) {
+			TreeNode temp = invertTree(root.left);
+			root.left = invertTree(root.right);
+			root.right = temp;
+		}
 		return root;
-	}
-}
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode() {
-	}
-
-	TreeNode(int val) {
-		this.val = val;
-	}
-
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
 	}
 }
