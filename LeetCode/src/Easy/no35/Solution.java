f1333d93c449ccfe2f6ee121f4f4221d1f549de2
jang <jhbsp@naver.com>
2023-02-20 09:15:06 +0900

    230220

--- LeetCode/src/Easy/no35/Solution.java
+++ LeetCode/src/Easy/no35/Solution.java
@@ -1,13 +1,15 @@
 package Easy.no35;
 
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        
-        for(int i = 0 ; i<nums.length ; i++){
-            if(target<=nums[i])
-                return i;
-        }
-        
-        return nums.length;
-    }
+	public int searchInsert(int[] nums, int target) {
+		int head = -1, tail = nums.length;
+		while (head + 1 < tail) {
+			int mid = (head + tail) / 2;
+			if (nums[mid] < target)
+				head = mid;
+			else
+				tail = mid;
+		}
+		return tail;
+	}
 }
