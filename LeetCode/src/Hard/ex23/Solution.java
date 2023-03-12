9c495dd6d360c45b712eb592ca58cbc75681fc6d
jang <jhbsp@naver.com>
2023-03-12 10:45:47 +0900

    230312

--- LeetCode/src/Hard/ex23/Solution.java
+++ LeetCode/src/Hard/ex23/Solution.java
@@ -1,56 +1,21 @@
 package Hard.ex23;
 
+import java.util.PriorityQueue;
+
 class Solution {
 	public ListNode mergeKLists(ListNode[] lists) {
-
-		ListNode root = new ListNode();
-		ListNode head = root;
-		
-		while(hasNotNull(lists)) {
-			
-			int min = Integer.MAX_VALUE;
-			int index = -1;
-			
-			for(int i = 0 ; i<lists.length ; i++) {
-				if(lists[i] != null && lists[i].val < min) {
-					min = lists[i].val;
-					index = i;
-				}
-					
-			}
-			
-			head.next = lists[index];
+		PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
+		for (ListNode node : lists) {
+			if (node != null)
+				queue.add(node);
+		}
+		ListNode root = new ListNode(), head = root;
+		while (!queue.isEmpty()) {
+			head.next = queue.poll();
 			head = head.next;
-			lists[index] = lists[index].next;
-			
+			if (head.next != null)
+				queue.add(head.next);
 		}
-		
 		return root.next;
-	}
-
-	
-	public boolean hasNotNull(ListNode[] lists) {
-		for(ListNode node : lists) {
-			if(node != null)
-				return true;
-		}
-		return false;
-	}
-}
-
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode() {
-	}
-
-	ListNode(int val) {
-		this.val = val;
-	}
-
-	ListNode(int val, ListNode next) {
-		this.val = val;
-		this.next = next;
 	}
 }
