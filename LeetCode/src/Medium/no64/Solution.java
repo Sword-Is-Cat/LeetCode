521b59975a6474811298afc22562833af9183793
jang <jhbsp@naver.com>
2023-03-27 09:15:36 +0900

    230327

--- LeetCode/src/Medium/no64/Solution.java
+++ LeetCode/src/Medium/no64/Solution.java
@@ -1,34 +1,23 @@
 package Medium.no64;
 
 class Solution {
-	
-	int[][] pathSum;
-
 	public int minPathSum(int[][] grid) {
 
-		pathSum = new int[grid.length][grid[0].length];
+		int rowLeng = grid.length, colLeng = grid[0].length;
 
-		for (int i = 0; i < grid.length; i++)
-			for (int j = 0; j < grid[0].length; j++)
-				pathSum[i][j] = findPathSum(i, j, grid);
+		for (int row = 0; row < rowLeng; row++) {
+			for (int col = 0; col < colLeng; col++) {
 
-		return pathSum[pathSum.length - 1][pathSum[0].length - 1];
-
-	}
-
-	int findPathSum(int i, int j, int[][] grid) {
-
-		int result = grid[i][j];
-
-		if (i == 0 && j == 0)
-			return result;
-		else if (i == 0)
-			result += pathSum[i][j - 1];
-		else if (j == 0)
-			result += pathSum[i - 1][j];
-		else
-			result += Math.min(pathSum[i - 1][j], pathSum[i][j - 1]);
-
-		return result;
+				if (row == 0 && col == 0)
+					continue;
+				else if (row == 0)
+					grid[row][col] += grid[row][col - 1];
+				else if (col == 0)
+					grid[row][col] += grid[row - 1][col];
+				else
+					grid[row][col] += Math.min(grid[row - 1][col], grid[row][col - 1]);
+			}
+		}
+		return grid[rowLeng - 1][colLeng - 1];
 	}
 }
---
+++ LeetCode/src/Medium/no64/Solution_.java
@@ -0,0 +1,34 @@
+package Medium.no64;
+
+class Solution_ {
+	
+	int[][] pathSum;
+
+	public int minPathSum(int[][] grid) {
+
+		pathSum = new int[grid.length][grid[0].length];
+
+		for (int i = 0; i < grid.length; i++)
+			for (int j = 0; j < grid[0].length; j++)
+				pathSum[i][j] = findPathSum(i, j, grid);
+
+		return pathSum[pathSum.length - 1][pathSum[0].length - 1];
+
+	}
+
+	int findPathSum(int i, int j, int[][] grid) {
+
+		int result = grid[i][j];
+
+		if (i == 0 && j == 0)
+			return result;
+		else if (i == 0)
+			result += pathSum[i][j - 1];
+		else if (j == 0)
+			result += pathSum[i - 1][j];
+		else
+			result += Math.min(pathSum[i - 1][j], pathSum[i][j - 1]);
+
+		return result;
+	}
+}
