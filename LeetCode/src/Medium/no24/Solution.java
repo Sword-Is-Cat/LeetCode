5d7e87f1603277b93ac2ffd56be2b51bf0bd873b
jang <jhbsp@naver.com>
2023-05-16 09:23:40 +0900

    230516

--- LeetCode/src/Medium/no24/Solution.java
+++ LeetCode/src/Medium/no24/Solution.java
@@ -22,20 +22,3 @@ class Solution {
 		return root.next;
 	}
 }
-
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode() {
-	}
-
-	ListNode(int val) {
-		this.val = val;
-	}
-
-	ListNode(int val, ListNode next) {
-		this.val = val;
-		this.next = next;
-	}
-}
