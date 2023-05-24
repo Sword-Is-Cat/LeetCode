b983718e60a69243fe6b880a779919970985c9d8
jang <jhbsp@naver.com>
2023-05-24 09:47:57 +0900

    230524

--- LeetCode/src/Medium/no2542/Solution.java
+++ LeetCode/src/Medium/no2542/Solution.java
@@ -1,21 +1,23 @@
 package Medium.no2542;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 class Solution {
 	public long maxScore(int[] nums1, int[] nums2, int k) {
 
 		long max = 0, sum = 0;
-		PriorityQueue<int[]> dataPool = new PriorityQueue<>((a, b) -> b[1] - a[1]),
-				entry = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+		int[][] dataPool = new int[nums1.length][];
+		PriorityQueue<int[]> entry = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
 		for (int i = 0; i < nums1.length; i++) {
-			dataPool.add(new int[] { nums1[i], nums2[i] });
+			dataPool[i] = new int[] { nums1[i], nums2[i] };
 		}
 
-		while (!dataPool.isEmpty()) {
+		Arrays.sort(dataPool, (a, b) -> b[1] - a[1]);
 
-			int[] data = dataPool.poll();
+		for (int[] data : dataPool) {
+
 			entry.add(data);
 			sum += data[0];
 
