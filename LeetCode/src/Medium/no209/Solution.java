b6ef6e9b5cb0bb722bb012808645dbbc66ce125a
jang <jhbsp@naver.com>
2023-07-06 09:18:44 +0900

    230706

--- LeetCode/src/Medium/no209/Solution.java
+++ LeetCode/src/Medium/no209/Solution.java
@@ -3,33 +3,18 @@ package Medium.no209;
 class Solution {
 	public int minSubArrayLen(int target, int[] nums) {
 
-		int left = 0, right = 0, sum = 0, ans = 0;
+		int left = 0, right = 0, sum = 0, ans = Integer.MAX_VALUE;
 
-		while (right < nums.length) {
+		while (right < nums.length || target <= sum) {
 
-			if (sum < target) {
+			if (sum < target && right < nums.length) {
 				sum += nums[right++];
 			} else {
-				if (ans == 0)
-					ans = right - left;
-				else
-					ans = Math.min(ans, right - left);
-
+				ans = Math.min(ans, right - left);
 				sum -= nums[left++];
 			}
-
 		}
 
-		while (sum >= target) {
-
-			if (ans == 0)
-				ans = right - left;
-			else
-				ans = Math.min(ans, right - left);
-
-			sum -= nums[left++];
-		}
-
-		return ans;
+		return ans == Integer.MAX_VALUE ? 0 : ans;
 	}
 }
