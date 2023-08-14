8fef5d21400e5408e551894b31792d517fb7ad8e
jang <jhbsp@naver.com>
2023-08-14 11:25:19 +0900

    230814

--- LeetCode/src/Medium/no215/Solution.java
+++ LeetCode/src/Medium/no215/Solution.java
@@ -1,13 +1,20 @@
 package Medium.no215;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-    	
-    	Arrays.sort(nums);
-    	
-    	return nums[nums.length-k];
-        
-    }
+	public int findKthLargest(int[] nums, int k) {
+
+		PriorityQueue<Integer> que = new PriorityQueue<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			que.add(nums[i]);
+			if (que.size() > k) {
+				que.poll();
+			}
+		}
+
+		return que.peek();
+
+	}
 }
