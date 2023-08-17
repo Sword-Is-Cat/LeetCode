7350e79aac13e7a17da5a957cb0741b37df02df2
jang <jhbsp@naver.com>
2023-08-17 09:47:09 +0900

    230817

--- LeetCode/src/Medium/no542/Solution.java
+++ LeetCode/src/Medium/no542/Solution.java
@@ -1,7 +1,6 @@
 package Medium.no542;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 
 class Solution {
@@ -9,26 +8,22 @@ class Solution {
 
 		int row = mat.length, col = mat[0].length;
 
-		int[][] ans = new int[row][col];
-		for (int i = 0; i < row; i++) {
-			Arrays.fill(ans[i], -1);
-		}
-
 		Queue<int[]> que = new ArrayDeque<>();
 
 		for (int r = 0; r < row; r++) {
 			for (int c = 0; c < col; c++) {
 				if (mat[r][c] == 0)
 					que.add(new int[] { r, c, 0 });
+				mat[r][c] = -1;
 			}
 		}
 
 		while (!que.isEmpty()) {
 			int[] info = que.poll();
 			int r = info[0], c = info[1], d = info[2];
-			if (ans[r][c] != -1 && ans[r][c] <= d)
+			if (mat[r][c] > -1 && mat[r][c] <= d)
 				continue;
-			ans[r][c] = d;
+			mat[r][c] = d;
 			if (0 < r)
 				que.add(new int[] { r - 1, c, d + 1 });
 			if (0 < c)
@@ -39,6 +34,6 @@ class Solution {
 				que.add(new int[] { r, c + 1, d + 1 });
 		}
 
-		return ans;
+		return mat;
 	}
 }
