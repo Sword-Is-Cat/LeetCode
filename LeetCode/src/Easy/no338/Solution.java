9cc502948a9f342406fbc81c80d43d4d1f9f41b2
jang <jhbsp@naver.com>
2023-09-01 09:15:16 +0900

    230901

--- LeetCode/src/Easy/no338/Solution.java
+++ LeetCode/src/Easy/no338/Solution.java
@@ -1,17 +1,20 @@
 package Easy.no338;
 
 class Solution {
-    public int[] countBits(int n) {
-        
-    	int[] answer = new int[n+1];
-    	
-    	for(int i = 0 ; i<=n ; i++) {
-    		
-    		answer[i] = Integer.toString(i, 2).replace("0", "").length();
-    		
-    	}
-    	
-    	return answer;
-    	
-    }
+	public int[] countBits(int n) {
+		int[] ans = new int[n + 1];
+		for (int i = 0; i < ans.length; i++) {
+			ans[i] = countBit(i);
+		}
+		return ans;
+	}
+
+	private int countBit(int num) {
+		int cnt = 0;
+		while (num > 0) {
+			cnt += num & 1;
+			num = num >> 1;
+		}
+		return cnt;
+	}
 }
---
+++ LeetCode/src/Easy/no338/Solution_.java
@@ -0,0 +1,17 @@
+package Easy.no338;
+
+class Solution_ {
+    public int[] countBits(int n) {
+        
+    	int[] answer = new int[n+1];
+    	
+    	for(int i = 0 ; i<=n ; i++) {
+    		
+    		answer[i] = Integer.toString(i, 2).replace("0", "").length();
+    		
+    	}
+    	
+    	return answer;
+    	
+    }
+}
