342b7ca1d41ac33c256781e6ea820e04e764a0a5
jang <jhbsp@naver.com>
2023-09-04 09:26:23 +0900

    230904

---
+++ LeetCode/src/Easy/no141/ListNode.java
@@ -0,0 +1,10 @@
+package Easy.no141;
+
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
--- LeetCode/src/Easy/no141/Solution.java
+++ LeetCode/src/Easy/no141/Solution.java
@@ -1,26 +1,17 @@
 package Easy.no141;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Solution {
     public boolean hasCycle(ListNode head) {
-    	if(head==null)return false;
-        Set<ListNode> set = new HashSet<ListNode>();
-        while(head.next!=null){
-            if(!set.add(head)) return true;
-            head = head.next;
+    	
+        ListNode slow = head, fast = head;
+        
+        while(fast!=null && fast.next!=null) {
+        	slow = slow.next;
+        	fast = fast.next.next;
+        	if(slow==fast)
+        		return true;
         }
+        
         return false;
     }
-}
-
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
 }
