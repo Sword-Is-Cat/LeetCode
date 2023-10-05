30e818bbec2ff918459588e254d61caf406d32ba
jang <jhbsp@naver.com>
2023-10-05 16:10:34 +0900

    231005

--- LeetCode/src/Medium/no229/Solution.java
+++ LeetCode/src/Medium/no229/Solution.java
@@ -1,26 +1,46 @@
 package Medium.no229;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
 	public List<Integer> majorityElement(int[] nums) {
 
-		HashSet<Integer> set = new HashSet<>();
-		int size = nums.length / 3;
+		int value1 = Integer.MAX_VALUE, cnt1 = 0, value2 = Integer.MAX_VALUE, cnt2 = 0;
 
-		Arrays.sort(nums);
-
-		for (int i = 0; i + size < nums.length; i++) {
-
-			if (nums[i] == nums[i + size])
-				set.add(nums[i]);
-
+		for (int num : nums) {
+			if (num == value1) {
+				cnt1++;
+			} else if (num == value2) {
+				cnt2++;
+			} else if (cnt1 == 0) {
+				value1 = num;
+				cnt1 = 1;
+			} else if (cnt2 == 0) {
+				value2 = num;
+				cnt2 = 1;
+			} else {
+				cnt1--;
+				cnt2--;
+			}
 		}
 
-		return set.stream().collect(Collectors.toList());
+		cnt1 = cnt2 = 0;
 
+		for (int num : nums) {
+			if (num == value1)
+				cnt1++;
+			else if (num == value2)
+				cnt2++;
+		}
+
+		List<Integer> list = new ArrayList<>();
+
+		if (cnt1 > nums.length / 3)
+			list.add(value1);
+		if (cnt2 > nums.length / 3)
+			list.add(value2);
+
+		return list;
 	}
 }
