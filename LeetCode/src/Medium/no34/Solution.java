cc7e8f7d767393724d6e1e44d722de4313621ec2
jang <jhbsp@naver.com>
2023-10-09 16:08:41 +0900

    231009

--- LeetCode/src/Medium/no34/Solution.java
+++ LeetCode/src/Medium/no34/Solution.java
@@ -8,42 +8,28 @@ class Solution {
 		if (nums.length == 0 || nums[0] > target || nums[nums.length - 1] < target)
 			return answer;
 
-		int minLeft = -1;
-		int minRight = nums.length;
-		int minMid = 0;
-
-		int maxLeft = -1;
-		int maxRight = nums.length;
-		int maxMid = 0;
+		int minLeft = -1, minRight = nums.length, minMid = 0;
 
 		while (minLeft < minRight - 1) {
-
 			minMid = (minLeft + minRight) / 2;
-
 			if (nums[minMid] < target)
 				minLeft = minMid;
 			else
 				minRight = minMid;
-
 		}
 
+		int maxLeft = -1, maxRight = nums.length, maxMid = 0;
 		while (maxLeft < maxRight - 1) {
-
 			maxMid = (maxLeft + maxRight) / 2;
-
 			if (nums[maxMid] > target)
 				maxRight = maxMid;
 			else
 				maxLeft = maxMid;
-
 		}
 
 		if (minLeft != -1 && nums[minRight] > target)
 			return answer;
 
-		answer[0] = minRight;
-		answer[1] = maxLeft;
-
-		return answer;
+		return new int[] { minRight, maxLeft };
 	}
 }
