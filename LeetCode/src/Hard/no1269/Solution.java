76da56c237fd182ac067b60e29e0f8f9dcb64d94
jang <jhbsp@naver.com>
2023-10-15 21:29:22 +0900

    231015

--- LeetCode/src/Hard/no1269/Solution.java
+++ LeetCode/src/Hard/no1269/Solution.java
@@ -8,16 +8,17 @@ class Solution {
 		int[][] dp = new int[2][arrLen];
 
 		dp[0][0] = 1;
-		int slide = 0;
+		int slide = 0, prev = 0;
 
 		while (steps-- > 0) {
+			prev = slide;
 			slide ^= 1;
 			for (int i = 0; i < arrLen; i++) {
-				dp[slide][i] = dp[slide ^ 1][i];
+				dp[slide][i] = dp[prev][i];
 				if (0 < i)
-					dp[slide][i] = (dp[slide][i] + dp[slide ^ 1][i - 1]) % mod;
+					dp[slide][i] = (dp[slide][i] + dp[prev][i - 1]) % mod;
 				if (i + 1 < arrLen)
-					dp[slide][i] = (dp[slide][i] + dp[slide ^ 1][i + 1]) % mod;
+					dp[slide][i] = (dp[slide][i] + dp[prev][i + 1]) % mod;
 			}
 		}
 
