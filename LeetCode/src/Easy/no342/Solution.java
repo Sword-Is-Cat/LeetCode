efa90ff2c6392e0056fe1b3a98253c1782a57c8b
jang <jhbsp@naver.com>
2023-10-23 12:23:41 +0900

    231023

--- LeetCode/src/Easy/no342/Solution.java
+++ LeetCode/src/Easy/no342/Solution.java
@@ -2,21 +2,6 @@ package Easy.no342;
 
 class Solution {
 	public boolean isPowerOfFour(int num) {
-
-		if (num < 1)
-			return false;
-
-		boolean answer = true;
-
-		while (num > 1) {
-			if (num % 4 != 0) {
-				answer = false;
-				break;
-			} else
-				num /= 4;
-		}
-
-		return answer;
-
+		return num == 0 ? false : num == 1 ? true : num % 4 == 0 ? isPowerOfFour(num / 4) : false;
 	}
 }
