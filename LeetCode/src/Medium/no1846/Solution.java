08ad12bf496f070caf0cd53b0c0b8f3f2c324e06
jang <jhbsp@naver.com>
2023-11-15 12:48:54 +0900

    231115

---
+++ LeetCode/src/Medium/no1846/Solution.java
@@ -0,0 +1,18 @@
+package Medium.no1846;
+
+import java.util.Arrays;
+
+class Solution {
+	public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
+		Arrays.sort(arr);
+		int value = 0;
+		
+		for(int no : arr) {
+			if(value<no)
+				value++;
+		}
+
+		return value;
+	}
+}
