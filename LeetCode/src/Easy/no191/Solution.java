4ab69829adc43e38602a966b91b388b8daac8e65
jang <jhbsp@naver.com>
2023-11-29 14:28:19 +0900

    231129

--- LeetCode/src/Easy/no191/Solution.java
+++ LeetCode/src/Easy/no191/Solution.java
@@ -1,17 +1,14 @@
 package Easy.no191;
 
 public class Solution {
-	// you need to treat n as an unsigned value
 	public int hammingWeight(int n) {
 
-		int answer = 0;
+		int loop = 32, ans = 0;
 
-		for (int i = 0; i < 32; i++) {
-			answer += n % 2;
+		while (loop-- > 0) {
+			ans += n & 1;
 			n >>= 1;
 		}
-
-		return answer > 0 ? answer : -answer;
-
+		return ans;
 	}
 }
