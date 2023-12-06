d6efb120e59cddc0e91f10bf0d2671ae04584715
jang <jhbsp@naver.com>
2023-12-06 14:33:54 +0900

    231206 refac

--- LeetCode/src/Easy/no1716/Solution.java
+++ LeetCode/src/Easy/no1716/Solution.java
@@ -3,16 +3,10 @@ package Easy.no1716;
 class Solution {
 	public int totalMoney(int n) {
 
-		int[] sums = { 0, 1, 3, 6, 10, 15, 21 };
+		int[] accum = { 0, 1, 3, 6, 10, 15, 21 };
+		int weeks = n / 7, dates = n % 7;
 
-		int fullWeeks = n / 7, dates = n % 7;
-
-		int sum = 0;
-
-		sum += fullWeeks * 28 + fullWeeks * (fullWeeks - 1) * 7 / 2;
-		sum += sums[dates] + dates * fullWeeks;
-
-		return sum;
+		return weeks * 28 + weeks * (weeks - 1) * 7 / 2 + accum[dates] + dates * weeks;
 
 	}
 }
