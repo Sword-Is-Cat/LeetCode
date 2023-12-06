ac28cbaffd68f644c672b3449477adc5fd82efbb
jang <jhbsp@naver.com>
2023-12-06 14:52:52 +0900

    account test

--- LeetCode/src/Easy/no1716/Solution.java
+++ LeetCode/src/Easy/no1716/Solution.java
@@ -6,7 +6,7 @@ class Solution {
 		int[] accum = { 0, 1, 3, 6, 10, 15, 21 };
 		int weeks = n / 7, dates = n % 7;
 
-		return weeks * 28 + weeks * (weeks - 1) * 7 / 2 + accum[dates] + dates * weeks;
+		return weeks * 28 + weeks * (weeks - 1) * 7 / 2 + accum[dates] + dates * weeks;//
 
 	}
 }
