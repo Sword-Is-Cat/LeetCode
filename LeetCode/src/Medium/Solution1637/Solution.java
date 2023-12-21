b9b57afc7e48696b51f11b1fc6643f544ce4c464
jang <jhbsp@naver.com>
2023-12-21 16:25:46 +0900

    231221

---
+++ LeetCode/src/Medium/Solution1637/Solution.java
@@ -0,0 +1,18 @@
+package Medium.Solution1637;
+
+import java.util.Arrays;
+
+class Solution {
+	public int maxWidthOfVerticalArea(int[][] points) {
+
+		Arrays.sort(points, (a, b) -> a[0] - b[0]);
+
+		int ans = 0;
+
+		for (int i = 1; i < points.length; i++) {
+			ans = Math.max(ans, points[i][0] - points[i - 1][0]);
+		}
+
+		return ans;
+	}
+}
