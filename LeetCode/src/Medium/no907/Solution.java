8dbf09a48986939d4bd8d8470dfb93279e8afb5a
jang <jhbsp@naver.com>
2024-01-20 12:44:11 +0900

    240120

--- LeetCode/src/Medium/no907/Solution.java
+++ LeetCode/src/Medium/no907/Solution.java
@@ -5,19 +5,16 @@ import java.util.Stack;
 class Solution {
 	public int sumSubarrayMins(int[] arr) {
 		int ans = 0;
+		int mod = (int) 1e9 + 7;
 		Stack<Integer> stack = new Stack<>();
-		int mod = 1000000007;
 		stack.push(-1);
 		for (int i = 0; i < arr.length + 1; i++) {
 			int currVal = (i <= arr.length - 1) ? arr[i] : 0;
 			while (stack.peek() != -1 && currVal < arr[stack.peek()]) {
 				int index = stack.pop();
 				int j = stack.peek();
-				int left = index - j;
-				int right = i - index;
-				long add = (left * right * (long) arr[index]) % mod;
-				ans += add;
-				ans %= mod;
+				long left = index - j, right = i - index;
+				ans = (ans + (int) ((left * right * arr[index]) % mod)) % mod;
 			}
 			stack.push(i);
 		}
