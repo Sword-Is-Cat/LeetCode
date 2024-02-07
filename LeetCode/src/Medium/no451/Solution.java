b6037296e7df4731ae80532552c7e1dbc8d44705
jang <jhbsp@naver.com>
2024-02-07 23:16:16 +0900

    240207

--- LeetCode/src/Medium/no451/Solution.java
+++ LeetCode/src/Medium/no451/Solution.java
@@ -1,31 +1,47 @@
 package Medium.no451;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
 	public String frequencySort(String s) {
-		HashMap<Character, StringBuilder> map = new HashMap<>();
-		ArrayList<StringBuilder> list = new ArrayList<>();
 
-		for (char ch : s.toCharArray()) {
-
-			if (!map.containsKey(ch)) {
-				StringBuilder sb = new StringBuilder();
-				map.put(ch, sb);
-				list.add(sb);
-			}
-
-			map.get(ch).append(ch);
-
+		int[][] counter = new int[62][2];
+		for (int i = 0; i < counter.length; i++) {
+			counter[i][0] = i;
 		}
 
-		StringBuilder ans = new StringBuilder();
-		list.sort((a, b) -> b.length() - a.length());
+		for (char ch : s.toCharArray()) {
+			counter[charToInt(ch)][1]++;
+		}
 
-		for (StringBuilder sb : list)
-			ans.append(sb);
+		Arrays.sort(counter, (a, b) -> b[1] - a[1]);
 
-		return ans.toString();
+		StringBuilder sb = new StringBuilder();
+
+		for (int[] entry : counter) {
+			while (entry[1]-- > 0) {
+				sb.append(intToChar(entry[0]));
+			}
+		}
+
+		return sb.toString();
+	}
+
+	private int charToInt(char ch) {
+		if (Character.isLowerCase(ch))
+			return ch - 'a' + 10;
+		else if (Character.isUpperCase(ch))
+			return ch - 'A' + 36;
+		else
+			return ch - '0';
+	}
+
+	private char intToChar(int i) {
+		if (i < 10)
+			return (char) ('0' + i);
+		else if (i < 36)
+			return (char) ('a' - 10 + i);
+		else
+			return (char) ('A' - 36 + i);
 	}
 }
