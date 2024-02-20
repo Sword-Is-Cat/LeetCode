4a9f7de81d3c5a9606f49835c499fe4e6ce28c02
jang <jhbsp@naver.com>
2024-02-20 16:11:04 +0900

    240220

--- LeetCode/src/Easy/no268/Solution.java
+++ LeetCode/src/Easy/no268/Solution.java
@@ -2,20 +2,10 @@ package Easy.no268;
 
 class Solution {
 	public int missingNumber(int[] nums) {
-
-		boolean[] arr = new boolean[nums.length + 1];
-
-		for (int i : nums)
-			arr[i] = true;
-
-		int answer = 0;
-
-		for (int i = 0; i < arr.length; i++)
-			if (!arr[i]) {
-				answer = i;
-				break;
-			}
-
-		return answer;
+		int num = nums.length;
+		for (int i = 0; i < nums.length; i++) {
+			num ^= i ^ nums[i];
+		}
+		return num;
 	}
 }
---
+++ LeetCode/src/Easy/no268/Solution_.java
@@ -0,0 +1,21 @@
+package Easy.no268;
+
+class Solution_ {
+	public int missingNumber(int[] nums) {
+
+		boolean[] arr = new boolean[nums.length + 1];
+
+		for (int i : nums)
+			arr[i] = true;
+
+		int answer = 0;
+
+		for (int i = 0; i < arr.length; i++)
+			if (!arr[i]) {
+				answer = i;
+				break;
+			}
+
+		return answer;
+	}
+}
