c5e7c35f95cd44bb136416dc64173ed55102e3d4
jang <jhbsp@naver.com>
2024-02-22 14:02:57 +0900

    240222

--- LeetCode/src/Easy/no997/Solution.java
+++ LeetCode/src/Easy/no997/Solution.java
@@ -1,29 +1,22 @@
 package Easy.no997;
 
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Solution {
 	public int findJudge(int n, int[][] trust) {
 
-		if (n == 1)
-			return 1;
+		int[] trusting = new int[n + 1];
+		int[] trusted = new int[n + 1];
 
-		HashMap<Integer, HashSet<Integer>> trusting = new HashMap<>();
-		HashMap<Integer, HashSet<Integer>> trusted = new HashMap<>();
-
-		for (int[] trus : trust) {
-
-			int a = trus[0], b = trus[1];
-
-			trusting.computeIfAbsent(a, v -> new HashSet<>()).add(b);
-			trusted.computeIfAbsent(b, v -> new HashSet<>()).add(a);
+		for (int[] info : trust) {
+			trusting[info[0]]++;
+			trusted[info[1]]++;
 		}
 
-		for (int i = 1; i <= n; i++) {
-			if (!trusting.containsKey(i) && trusted.containsKey(i) && trusted.get(i).size() == n - 1)
+		for (int i = 1; i < trusted.length; i++) {
+			if (trusting[i] == 0 && trusted[i] == n - 1)
 				return i;
 		}
+
 		return -1;
+
 	}
 }
