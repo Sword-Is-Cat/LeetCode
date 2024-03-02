642f8b94b187354a996905280c5dfdd6356691b1
jang <jhbsp@naver.com>
2024-03-02 11:25:44 +0900

    240302

--- LeetCode/src/Easy/no977/Solution.java
+++ LeetCode/src/Easy/no977/Solution.java
@@ -5,31 +5,20 @@ class Solution {
 
 		int[] answer = new int[nums.length];
 
-		int minusIndex = nums.length - 1;
+		int left = 0, right = nums.length - 1, idx = nums.length - 1;
 
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] > 0) {
-				minusIndex = i - 1;
-				break;
+		while (left <= right) {
+			if (nums[left] * nums[left] < nums[right] * nums[right]) {
+				answer[idx] = nums[right] * nums[right];
+				right--;
+			} else {
+				answer[idx] = nums[left] * nums[left];
+				left++;
 			}
+			idx--;
 		}
 
-		int plusIndex = minusIndex + 1;
-
-		for (int i = 0; i < answer.length; i++)
-
-			if (minusIndex < 0)
-				answer[i] = nums[plusIndex] * nums[plusIndex++];
-
-			else if (plusIndex >= answer.length)
-				answer[i] = nums[minusIndex] * nums[minusIndex--];
-
-			else if (nums[minusIndex] * nums[minusIndex] > nums[plusIndex] * nums[plusIndex])
-				answer[i] = nums[plusIndex] * nums[plusIndex++];
-
-			else
-				answer[i] = nums[minusIndex] * nums[minusIndex--];
-
 		return answer;
+
 	}
 }
