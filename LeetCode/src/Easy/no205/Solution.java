f7076bbc2e013c7cc338da18e629b324cf4be0eb
jang <jhbsp@naver.com>
2024-04-02 11:02:26 +0900

    240402

--- LeetCode/src/Easy/no205/Solution.java
+++ LeetCode/src/Easy/no205/Solution.java
@@ -1,40 +1,21 @@
 package Easy.no205;
 
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
 	public boolean isIsomorphic(String s, String t) {
 
-		char[] sArr = s.toCharArray();
-		char[] tArr = t.toCharArray();
+		HashMap<Character, Character> map = new HashMap<>();
+		HashMap<Character, Character> revMap = new HashMap<>();
 
-		Map<Character, Integer> sMap = new HashMap<>();
-		Map<Character, Integer> tMap = new HashMap<>();
-
-		boolean result = sArr.length == tArr.length;
-
-		if (result) {
-
-			for (int i = 0; i < sArr.length; i++) {
-
-				if (!sMap.containsKey(sArr[i])) {
-					sMap.put(sArr[i], i);
-				}
-				if (!tMap.containsKey(tArr[i])) {
-					tMap.put(tArr[i], i);
-				}
-
-			}
-
-			for (int i = 0; i < sArr.length; i++) {
-				if (sMap.get(sArr[i]) != tMap.get(tArr[i])) {
-					return false;
-				}
-			}
-
+		for (int i = 0; i < s.length(); i++) {
+			char ori = s.charAt(i), chng = t.charAt(i);
+			map.putIfAbsent(ori, chng);
+			revMap.putIfAbsent(chng, ori);
+			if (map.get(ori) != chng || revMap.get(chng) != ori)
+				return false;
 		}
 
-		return result;
+		return true;
 	}
 }
