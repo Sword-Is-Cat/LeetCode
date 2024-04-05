af1dbd4c0fb20968dab5e03a891f614674cbebdf
jang <jhbsp@naver.com>
2024-04-05 14:28:50 +0900

    240405

--- LeetCode/src/Easy/no1544/Solution_.java
+++ LeetCode/src/Easy/no1544/Solution_.java
@@ -2,22 +2,30 @@ package Easy.no1544;
 
 import java.util.Stack;
 
-class Solution {
+class Solution_ {
 	public String makeGood(String s) {
-
 		Stack<Character> stack = new Stack<>();
-		int gap = Math.abs('A' - 'a');
+
 		for (char ch : s.toCharArray()) {
-			if (stack.isEmpty() || Math.abs(stack.peek() - ch) != gap)
-				stack.add(ch);
-			else
+
+			if (!stack.isEmpty() && isBad(ch, stack.peek())) {
 				stack.pop();
+			} else {
+				stack.add(ch);
+			}
+
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
-		while(!stack.isEmpty())
+
+		while (!stack.isEmpty())
 			sb.append(stack.pop());
 
 		return sb.reverse().toString();
+
+	}
+
+	public boolean isBad(char ch1, char ch2) {
+		return ch1 != ch2 && Character.toUpperCase(ch1) == Character.toUpperCase(ch2);
 	}
 }
