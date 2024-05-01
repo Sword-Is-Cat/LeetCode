b04f88f8788897324c9aa213e5503d98496f25a7
jang <jhbsp@naver.com>
2024-05-01 12:37:23 +0900

    240501

--- LeetCode/src/Easy/no2000/Solution.java
+++ LeetCode/src/Easy/no2000/Solution.java
@@ -3,10 +3,8 @@ package Easy.no2000;
 class Solution {
 	public String reversePrefix(String word, char ch) {
 
-		int idx = word.indexOf(ch);
-		if (ch == -1)
-			return word;
+		int idx = word.indexOf(ch) + 1;
 
-		return new StringBuilder().append(word.substring(0, ++idx)).reverse().append(word.substring(idx)).toString();
+		return new StringBuilder().append(word.substring(0, idx)).reverse().append(word.substring(idx)).toString();
 	}
 }
