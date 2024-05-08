674b94a24bc71c4a8330255e35f4e9dc733e6337
jang <jhbsp@naver.com>
2024-05-08 10:23:50 +0900

    240508

--- LeetCode/src/Easy/no506/Solution.java
+++ LeetCode/src/Easy/no506/Solution.java
@@ -1,30 +1,35 @@
 package Easy.no506;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 	public String[] findRelativeRanks(int[] nums) {
 
-		int[] ranked = nums.clone();
-		Arrays.sort(ranked);
-
 		String[] answer = new String[nums.length];
+		Map<Integer, String> map = pointToRankMap(nums);
 
-		for (int i = 1; i <= nums.length; i++) {
-			for (int j = 0; j < nums.length; j++) {
-				if (ranked[ranked.length - i] == nums[j]) {
-					if (i == 1)
-						answer[j] = "Gold Medal";
-					else if (i == 2)
-						answer[j] = "Silver Medal";
-					else if (i == 3)
-						answer[j] = "Bronze Medal";
-					else
-						answer[j] = i + "";
-				}
-			}
+		for (int i = 0; i < nums.length; i++) {
+			answer[i] = map.get(nums[i]);
 		}
 
 		return answer;
+	}
+
+	private Map<Integer, String> pointToRankMap(int[] nums) {
+
+		Map<Integer, String> map = new HashMap<>();
+		int[] rank = nums.clone();
+		Arrays.sort(rank);
+
+		String[] example = { "", "Gold Medal", "Silver Medal", "Bronze Medal" };
+
+		for (int i = 1; i <= rank.length; i++) {
+			map.put(rank[rank.length - i], i < 4 ? example[i] : (i + ""));
+		}
+
+		return map;
+
 	}
 }
