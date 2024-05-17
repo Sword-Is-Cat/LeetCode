b37aad8400e413c2bce12d6532b4ad2923699711
jang <jhbsp@naver.com>
2024-05-17 13:54:53 +0900

    240517

---
+++ LeetCode/src/Medium/no1325/Solution.java
@@ -0,0 +1,17 @@
+package Medium.no1325;
+
+class Solution {
+	public TreeNode removeLeafNodes(TreeNode root, int target) {
+
+		if (root != null) {
+			
+			root.left = removeLeafNodes(root.left, target);
+			root.right = removeLeafNodes(root.right, target);
+
+			if (root.left == null && root.right == null && root.val == target) {
+				return null;
+			}
+		}
+		return root;
+	}
+}
