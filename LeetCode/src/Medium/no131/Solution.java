af2678bcb9a6cc16dfdd811295740ec9c837b236
jang <jhbsp@naver.com>
2024-05-22 13:19:25 +0900

    240522

--- LeetCode/src/Medium/no131/Solution.java
+++ LeetCode/src/Medium/no131/Solution.java
@@ -4,55 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-
-	List<List<String>> ans = new ArrayList<>();
-	String str;
-	char[] arr;
-	boolean[][] isPalin;
-
 	public List<List<String>> partition(String s) {
 
-		str = s;
-		arr = s.toCharArray();
-		isPalin = new boolean[arr.length][arr.length];
-
-		for (int left = 0; left < arr.length; left++) {
-			for (int right = left; right < arr.length; right++) {
-				isPalin[left][right] = isPalindrome(left, right);
-			}
-		}
-
-		dfs(new ArrayList<>(), 0, 0);
-
+		boolean[][] table = getBooleanTable(s);
+		List<List<String>> ans = new ArrayList<>();
+		dfs(ans, new ArrayList<>(), s, table, 0);
 		return ans;
 	}
 
-	protected void dfs(List<String> list, int prev, int curr) {
-
-		if (curr == arr.length) {
-			if (prev == curr)
-				ans.add(list);
-			return;
+	private void dfs(List<List<String>> ans, List<String> item, String str, boolean[][] table, int idx) {
+		if (idx == str.length()) {
+			ans.add(item);
+		} else {
+			for (int end = str.length() - 1; end >= idx; end--) {
+				if (table[idx][end]) {
+					List<String> next = idx == end ? item : new ArrayList<>(item);
+					next.add(str.substring(idx, end + 1));
+					dfs(ans, next, str, table, end + 1);
+				}
+			}
 		}
-
-		// [1] palindrome 이라면 list추가하는 case 처리
-		if (isPalin[prev][curr]) {
-			List<String> clone = new ArrayList<>(list);
-			clone.add(new String(str.substring(prev, curr + 1)));
-			dfs(clone, curr + 1, curr + 1);
-		}
-		// [2] list 추가없이 진행
-		dfs(list, prev, curr + 1);
 	}
 
-	protected boolean isPalindrome(int left, int right) {
-
-		while (left < right) {
-			if (arr[left] != arr[right])
-				return false;
-			left++;
-			right--;
+	private boolean[][] getBooleanTable(String str) {
+		int length = str.length();
+		boolean[][] result = new boolean[length][length];
+		for (int leng = 0; leng < length; leng++) {
+			for (int from = 0; from + leng < length; from++) {
+				if (leng == 0) {
+					result[from][from + leng] = true;
+				} else if (leng == 1) {
+					result[from][from + leng] = str.charAt(from) == str.charAt(from + leng);
+				} else {
+					result[from][from + leng] = str.charAt(from) == str.charAt(from + leng)
+							&& result[from + 1][from + leng - 1];
+				}
+			}
 		}
-		return true;
+		return result;
 	}
 }
---
+++ LeetCode/src/Medium/no131/Solution_.java
@@ -0,0 +1,58 @@
+package Medium.no131;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_ {
+
+	List<List<String>> ans = new ArrayList<>();
+	String str;
+	char[] arr;
+	boolean[][] isPalin;
+
+	public List<List<String>> partition(String s) {
+
+		str = s;
+		arr = s.toCharArray();
+		isPalin = new boolean[arr.length][arr.length];
+
+		for (int left = 0; left < arr.length; left++) {
+			for (int right = left; right < arr.length; right++) {
+				isPalin[left][right] = isPalindrome(left, right);
+			}
+		}
+
+		dfs(new ArrayList<>(), 0, 0);
+
+		return ans;
+	}
+
+	protected void dfs(List<String> list, int prev, int curr) {
+
+		if (curr == arr.length) {
+			if (prev == curr)
+				ans.add(list);
+			return;
+		}
+
+		// [1] palindrome 이라면 list추가하는 case 처리
+		if (isPalin[prev][curr]) {
+			List<String> clone = new ArrayList<>(list);
+			clone.add(new String(str.substring(prev, curr + 1)));
+			dfs(clone, curr + 1, curr + 1);
+		}
+		// [2] list 추가없이 진행
+		dfs(list, prev, curr + 1);
+	}
+
+	protected boolean isPalindrome(int left, int right) {
+
+		while (left < right) {
+			if (arr[left] != arr[right])
+				return false;
+			left++;
+			right--;
+		}
+		return true;
+	}
+}
