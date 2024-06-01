54869a77fc30fbc7646163831473d375793a600f
jang <jhbsp@naver.com>
2024-06-01 19:11:34 +0900

    240601

---
+++ LeetCode/src/Easy/no3110/Solution.java
@@ -0,0 +1,15 @@
+package Easy.no3110;
+
+class Solution {
+	public int scoreOfString(String s) {
+
+		int ans = 0;
+
+		for (int i = 1; i < s.length(); i++) {
+			ans += Math.abs(s.charAt(i) - s.charAt(i - 1));
+		}
+
+		return ans;
+
+	}
+}
