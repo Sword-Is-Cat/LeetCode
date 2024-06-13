de8b275587c7d19350911852d07211c0b147e925
jang <jhbsp@naver.com>
2024-06-13 13:01:45 +0900

    240613

--- LeetCode/src/Easy/no2037/Solution.java
+++ LeetCode/src/Easy/no2037/Solution.java
@@ -3,31 +3,17 @@ package Easy.no2037;
 class Solution {
 	public int minMovesToSeat(int[] seats, int[] students) {
 
-		int[] cntSeat = new int[101], cntStd = new int[101];
-		int ans = 0;
+		int[] diff = new int[101];
 
-		for (int seat : seats) {
-			cntSeat[seat]++;
-		}
-		for (int std : students) {
-			cntStd[std]++;
+		for (int i = 0; i < seats.length; i++) {
+			diff[seats[i]]++;
+			diff[students[i]]--;
 		}
 
-		int i = 0, j = 0;
-		while (i < 101 && j < 101) {
-			while (i < 101 && cntSeat[i] == 0) {
-				i++;
-			}
-			while (j < 101 && cntStd[j] == 0) {
-				j++;
-			}
-
-			if (i == 101 || j == 101)
-				break;
-
-			ans += Math.abs(i - j);
-			cntSeat[i]--;
-			cntStd[j]--;
+		int ans = 0, temp = 0;
+		for (int dif : diff) {
+			ans += Math.abs(temp);
+			temp += dif;
 		}
 
 		return ans;
