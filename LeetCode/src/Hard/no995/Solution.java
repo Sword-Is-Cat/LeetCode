d411372e19a4063c193bd4221ad1cdfceb1fd94c
jang <jhbsp@naver.com>
2024-06-24 13:45:01 +0900

    240624

--- LeetCode/src/Hard/no995/Solution.java
+++ LeetCode/src/Hard/no995/Solution.java
@@ -1,27 +1,23 @@
 package Hard.no995;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
 	public int minKBitFlips(int[] nums, int k) {
 
-		int ans = 0;
-		Queue<Integer> flipIndex = new LinkedList<>();
+		int ans = 0, flip = 0;
 
 		for (int i = 0; i < nums.length; i++) {
 
-			while (!flipIndex.isEmpty() && flipIndex.peek() <= i - k) {
-				flipIndex.poll();
+			if (k <= i && nums[i - k] == -1) {
+				flip ^= 1;
 			}
 
-			if (flipIndex.size() % 2 == nums[i]) {
+			if (nums[i] == flip) {
 				if (i + k > nums.length) {
 					return -1;
-				} else {
-					flipIndex.add(i);
-					ans++;
 				}
+				flip ^= 1;
+				ans++;
+				nums[i] = -1;
 			}
 
 		}
