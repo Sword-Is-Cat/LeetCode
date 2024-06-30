b235c6579040358efe227f24b866656996f4827c
jang <jhbsp@naver.com>
2024-06-30 13:30:56 +0900

    240630

--- LeetCode/src/Hard/no1579/Solution.java
+++ LeetCode/src/Hard/no1579/Solution.java
@@ -1,47 +1,42 @@
 package Hard.no1579;
 
-import java.util.Arrays;
-
 class Solution {
 
 	public int maxNumEdgesToRemove(int n, int[][] edges) {
-
-		Arrays.sort(edges, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
 
 		UnionFind auf = new UnionFind(n + 1), buf = new UnionFind(n + 1);
 
 		int cut = 0;
 
 		for (int[] edge : edges) {
-
 			int type = edge[0], s = edge[1], e = edge[2];
-
-			switch (type) {
-			case 1:
-				if (auf.checkConnect(s, e)) {
-					cut++;
-				} else {
-					auf.setUnion(s, e);
-				}
-				break;
-			case 2:
-				if (buf.checkConnect(s, e)) {
-					cut++;
-				} else {
-					buf.setUnion(s, e);
-				}
-				break;
-			case 3:
-
+			if (type == 3) {
 				if (auf.checkConnect(s, e) && buf.checkConnect(s, e)) {
 					cut++;
 				} else {
 					auf.setUnion(s, e);
 					buf.setUnion(s, e);
 				}
-				break;
 			}
+		}
 
+		for (int[] edge : edges) {
+
+			int type = edge[0], s = edge[1], e = edge[2];
+
+			if (type == 1) {
+				if (auf.checkConnect(s, e)) {
+					cut++;
+				} else {
+					auf.setUnion(s, e);
+				}
+			} else if (type == 2) {
+				if (buf.checkConnect(s, e)) {
+					cut++;
+				} else {
+					buf.setUnion(s, e);
+				}
+			}
 		}
 
 		for (int i = 1; i <= n; i++) {
