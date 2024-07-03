5d49fb1ab88185707b89352d208dcce6251cb520
jang <jhbsp@naver.com>
2024-07-03 13:41:48 +0900

    240703

--- LeetCode/src/Medium/no1509/Solution.java
+++ LeetCode/src/Medium/no1509/Solution.java
@@ -8,14 +8,47 @@ class Solution {
 		if (nums.length < 4)
 			return 0;
 
-		Arrays.sort(nums);
+		int[] min = getFourMins(nums);
+		int[] max = getFourMaxs(nums);
 
-		int dif1 = nums[nums.length - 4] - nums[0];
-		int dif2 = nums[nums.length - 3] - nums[1];
-		int dif3 = nums[nums.length - 2] - nums[2];
-		int dif4 = nums[nums.length - 1] - nums[3];
-
-		return Math.min(Math.min(dif1, dif2), Math.min(dif3, dif4));
+		return Math.min(Math.min(max[3] - min[0], max[2] - min[1]), Math.min(max[1] - min[2], max[0] - min[3]));
 
 	}
+
+	private int[] getFourMins(int[] nums) {
+		int[] min = new int[4];
+		Arrays.fill(min, Integer.MAX_VALUE);
+		for (int num : nums) {
+			for (int i = 0; i < 4; i++) {
+				if (min[i] >= num) {
+					min[i] = i == 3 ? num : min[i + 1];
+				} else {
+					if (i != 0) {
+						min[i - 1] = num;
+					}
+					break;
+				}
+			}
+		}
+		return min;
+	}
+
+	private int[] getFourMaxs(int[] nums) {
+		int[] max = new int[4];
+		Arrays.fill(max, Integer.MIN_VALUE);
+		for (int num : nums) {
+			for (int i = 0; i < 4; i++) {
+				if (max[i] <= num) {
+					max[i] = i == 3 ? num : max[i + 1];
+				} else {
+					if (i != 0) {
+						max[i - 1] = num;
+					}
+					break;
+				}
+			}
+		}
+		return max;
+	}
+
 }
