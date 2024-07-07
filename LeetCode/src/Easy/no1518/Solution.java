1ac1e5618a128314be2755b46b9cc7b4ab8d5425
jang <jhbsp@naver.com>
2024-07-07 22:44:12 +0900

    240707

---
+++ LeetCode/src/Easy/no1518/Solution.java
@@ -0,0 +1,17 @@
+package Easy.no1518;
+
+class Solution {
+	public int numWaterBottles(int numBottles, int numExchange) {
+
+		int ans = numBottles;
+
+		while (numBottles >= numExchange) {
+
+			ans += numBottles / numExchange;
+			numBottles = numBottles / numExchange + numBottles % numExchange;
+
+		}
+
+		return ans;
+	}
+}
