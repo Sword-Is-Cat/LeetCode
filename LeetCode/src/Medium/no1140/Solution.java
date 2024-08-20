7675ab1ad1456671ac48a732d883f174463fbcb9
jang <jhbsp@naver.com>
2024-08-20 13:07:14 +0900

    240820

--- LeetCode/src/Medium/no1140/Solution.java
+++ LeetCode/src/Medium/no1140/Solution.java
@@ -2,36 +2,30 @@ package Medium.no1140;
 
 class Solution {
 
-	int length;
-	int[] rSums;
-	int[][] dp;
-
 	public int stoneGameII(int[] piles) {
 
-		length = piles.length;
-
-		rSums = new int[length + 1];
-		for (int i = length - 1; 0 <= i; i--)
+		int[] rSums = new int[piles.length + 1];
+		for (int i = piles.length - 1; 0 <= i; i--)
 			rSums[i] = rSums[i + 1] + piles[i];
 
-		dp = new int[length][length + 1];
+		int[][] dp = new int[piles.length][piles.length + 1];
 
-		return expect(0, 1);
+		return expect(0, 1, rSums, dp);
 	}
 
-	private int expect(int idx, int m) {
+	private int expect(int idx, int m, int[] rSums, int[][] dp) {
 
 		if (dp[idx][m] == 0) {
 
 			int value = 0;
 
-			if (length <= idx + 2 * m) {
+			if (dp.length <= idx + 2 * m) {
 
 				value = rSums[idx];
 			} else {
 
 				for (int x = 1; x <= 2 * m; x++)
-					value = Math.max(value, rSums[idx] - expect(idx + x, Math.max(m, x)));
+					value = Math.max(value, rSums[idx] - expect(idx + x, Math.max(m, x), rSums, dp));
 
 			}
 			dp[idx][m] = value;
