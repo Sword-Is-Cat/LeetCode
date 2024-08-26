b8d793720ab36b6ecfc3a4a74159fcb957f4f50f
jang <jhbsp@naver.com>
2024-08-26 12:08:45 +0900

    240826

--- LeetCode/src/Easy/no590/Solution.java
+++ LeetCode/src/Easy/no590/Solution.java
@@ -5,43 +5,24 @@ import java.util.List;
 
 class Solution {
 
-	List<Integer> list = new ArrayList<>();
-
 	public List<Integer> postorder(Node root) {
 
-		process(root);
+		List<Integer> list = new ArrayList<>();
+		postorder(root, list);
 
 		return list;
 
 	}
 
-	void process(Node node) {
+	private void postorder(Node node, List<Integer> list) {
 
 		if (node == null)
 			return;
 
-		if (node.children.size() != 0)
-			for (Node child : node.children)
-				process(child);
+		for (Node child : node.children)
+			postorder(child, list);
 
 		list.add(node.val);
 
 	}
 }
-
-class Node {
-	public int val;
-	public List<Node> children;
-
-	public Node() {
-	}
-
-	public Node(int _val) {
-		val = _val;
-	}
-
-	public Node(int _val, List<Node> _children) {
-		val = _val;
-		children = _children;
-	}
-};
