ce1b3ba7f98a932dfa7f653f98b3d31945fa0377
jang <jhbsp@naver.com>
2024-08-27 13:32:02 +0900

    240827

--- LeetCode/src/Medium/no1514/Solution.java
+++ LeetCode/src/Medium/no1514/Solution.java
@@ -1,41 +1,45 @@
 package Medium.no1514;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 class Solution {
 	public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
 
-		HashMap<Integer, HashMap<Integer, Double>> map = new HashMap<>();
-		double[] probs = new double[n];
-
-		for (int i = 0; i < edges.length; i++) {
-			map.computeIfAbsent(edges[i][0], v -> new HashMap<>()).put(edges[i][1], succProb[i]);
-			map.computeIfAbsent(edges[i][1], v -> new HashMap<>()).put(edges[i][0], succProb[i]);
+		// edges, succProb의 graph화
+		List<Map<Integer, Double>> graph = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			graph.add(new HashMap<>());
 		}
+		for (int i = 0; i < edges.length; i++) {
+			graph.get(edges[i][0]).put(edges[i][1], succProb[i]);
+			graph.get(edges[i][1]).put(edges[i][0], succProb[i]);
+		}
+		
+		// 도착확률배열 probs선언 및 초기화
+		double[] probs = new double[n];
 		probs[start] = 1D;
 
-		PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> Double.compare(probs[b], probs[a]));
+		Queue<Integer> queue = new ArrayDeque<>();
 		queue.add(start);
 
 		while (!queue.isEmpty()) {
 
 			int point = queue.poll();
 
-			if (!map.containsKey(point))
-				continue;
+			for (int next : graph.get(point).keySet()) {
 
-			for (int next : map.get(point).keySet()) {
-
-				double newProb = probs[point] * map.get(point).get(next);
+				double newProb = probs[point] * graph.get(point).get(next);
 
 				if (probs[next] < newProb) {
 					probs[next] = newProb;
 					queue.add(next);
 				}
-
 			}
-
 		}
 
 		return probs[end];
