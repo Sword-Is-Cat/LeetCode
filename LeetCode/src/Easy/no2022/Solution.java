13430ffb6bca49a6d8e4ba521a3065a97d4ce651
jang <jhbsp@naver.com>
2024-09-01 11:56:20 +0900

    240901

--- LeetCode/src/Easy/no2022/Solution.java
+++ LeetCode/src/Easy/no2022/Solution.java
@@ -1,17 +1,18 @@
 package Easy.no2022;
 
-import java.util.Arrays;
-
 class Solution {
 	public int[][] construct2DArray(int[] original, int m, int n) {
 
 		if (original.length != m * n)
 			return new int[0][0];
 
-		int[][] ans = new int[m][];
+		int[][] ans = new int[m][n];
 
-		for (int row = 0; row < m; row++)
-			ans[row] = Arrays.copyOfRange(original, row * n, (row + 1) * n);
+		for (int r = 0, i = 0; r < m; r++) {
+			for (int c = 0; c < n; c++) {
+				ans[r][c] = original[i++];
+			}
+		}
 
 		return ans;
 	}
