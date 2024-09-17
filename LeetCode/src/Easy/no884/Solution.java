baf3dec7088dd71ff68d549e6d880089bcd171ec
jang <jhbsp@naver.com>
2024-09-17 11:47:30 +0900

    240917

--- LeetCode/src/Easy/no884/Solution.java
+++ LeetCode/src/Easy/no884/Solution.java
@@ -1,39 +1,27 @@
 package Easy.no884;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class Solution {
 	public String[] uncommonFromSentences(String s1, String s2) {
 
-		int length = 0;
-
 		Map<String, Integer> map = new HashMap<>();
-		for (String word : s1.split(" ")) {
-			map.put(word, map.getOrDefault(word, 0) + 1);
-			if (map.get(word) == 1) {
-				length++;
-			} else if (map.get(word) == 2) {
-				length--;
-			}
-		}
-		for (String word : s2.split(" ")) {
-			map.put(word, map.getOrDefault(word, 0) + 1);
-			if (map.get(word) == 1) {
-				length++;
-			} else if (map.get(word) == 2) {
-				length--;
-			}
-		}
 
-		String[] ans = new String[length];
-		int idx = 0;
+		for (String word : s1.split(" "))
+			map.put(word, map.getOrDefault(word, 0) + 1);
+
+		for (String word : s2.split(" "))
+			map.put(word, map.getOrDefault(word, 0) + 1);
+
+		List<String> ans = new ArrayList<>();
 		for (String word : map.keySet()) {
-			if (map.get(word) == 1) {
-				ans[idx++] = word;
-			}
+			if (map.get(word) == 1)
+				ans.add(word);
 		}
 
-		return ans;
+		return ans.toArray(new String[0]);
 	}
 }
