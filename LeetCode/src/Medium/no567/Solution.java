3323b02294363983c7e6b8f3694907567d3df0b9
jang <jhbsp@naver.com>
2024-10-05 12:56:25 +0900

    241005

--- LeetCode/src/Medium/no567/Solution.java
+++ LeetCode/src/Medium/no567/Solution.java
@@ -2,35 +2,35 @@ package Medium.no567;
 
 class Solution {
 
-	int[] cnt = new int[26];
-
 	public boolean checkInclusion(String s1, String s2) {
 
 		int length = s1.length();
-		for (char ch : s1.toCharArray())
+
+		int[] cnt = new int[26];
+		for (char ch : s1.toCharArray()) {
 			cnt[ch - 'a']++;
+		}
 
 		char[] chars = s2.toCharArray();
 		int[] arr = new int[26];
-
-		for (int i = 0; i < chars.length; i++) {
+		boolean answer = false;
+		for (int i = 0; !answer && i < chars.length; i++) {
 
 			arr[chars[i] - 'a']++;
-
 			if (length <= i)
 				arr[chars[i - length] - 'a']--;
 
-			if (query(arr))
-				return true;
+			answer |= arrayEqual(cnt, arr);
+
 		}
-		return false;
+		return answer;
 	}
 
-	protected boolean query(int[] arr) {
-		boolean rst = true;
-		for (int i = 0; rst && i < 26; i++) {
-			rst &= cnt[i] == arr[i];
+	protected boolean arrayEqual(int[] arr1, int[] arr2) {
+		boolean result = arr1.length == arr2.length;
+		for (int i = 0; result && i < arr1.length; i++) {
+			result &= arr1[i] == arr2[i];
 		}
-		return rst;
+		return result;
 	}
 }
