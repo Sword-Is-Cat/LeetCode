e236e81c2eb7e3e2df07d9a42e5b1abe3c9d7473
jang <jhbsp@naver.com>
2024-11-05 14:35:02 +0900

    241105

---
+++ LeetCode/src/Medium/no2914/Solution.java
@@ -0,0 +1,12 @@
+package Medium.no2914;
+
+class Solution {
+	public int minChanges(String s) {
+		int answer = 0;
+		for (int i = 0; i < s.length(); i += 2) {
+			if (s.charAt(i) != s.charAt(i + 1))
+				answer++;
+		}
+		return answer;
+	}
+}
