7fd9df9a3c208924c178aa85ac07cd5faffb8adb
jang <jhbsp@naver.com>
2024-11-06 11:13:04 +0900

    241106

--- LeetCode/src/Medium/no3011/Solution.java
+++ LeetCode/src/Medium/no3011/Solution.java
@@ -3,26 +3,26 @@ package Medium.no3011;
 class Solution {
 	public boolean canSortArray(int[] nums) {
 
-		for (int i = 1; i < nums.length; i++) {
+		boolean answer = true;
+
+		for (int i = 1; answer && i < nums.length; i++) {
 			if (nums[i - 1] > nums[i]) {
-				if (Integer.bitCount(nums[i - 1]) != Integer.bitCount(nums[i]))
-					return false;
+				answer &= Integer.bitCount(nums[i - 1]) == Integer.bitCount(nums[i]);
 				int temp = nums[i - 1];
 				nums[i - 1] = nums[i];
 				nums[i] = temp;
 			}
 		}
 
-		for (int i = nums.length - 1; i > 0; i--) {
+		for (int i = nums.length - 1; answer && i > 0; i--) {
 			if (nums[i - 1] > nums[i]) {
-				if (Integer.bitCount(nums[i - 1]) != Integer.bitCount(nums[i]))
-					return false;
+				answer &= Integer.bitCount(nums[i - 1]) == Integer.bitCount(nums[i]);
 				int temp = nums[i - 1];
 				nums[i - 1] = nums[i];
 				nums[i] = temp;
 			}
 		}
 
-		return true;
+		return answer;
 	}
 }
