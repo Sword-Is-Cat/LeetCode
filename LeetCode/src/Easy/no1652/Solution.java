4273d84192d10f54cf19f21c2cd471ea8ffaf2ad
jang <jhbsp@naver.com>
2024-11-18 11:25:12 +0900

    241118

--- LeetCode/src/Easy/no1652/Solution.java
+++ LeetCode/src/Easy/no1652/Solution.java
@@ -3,18 +3,25 @@ package Easy.no1652;
 class Solution {
 	public int[] decrypt(int[] code, int k) {
 
-		int[] ans = new int[code.length];
 		int length = code.length;
+		int[] ans = new int[length];
 
-		for (int i = 0; i < code.length; i++) {
-			if (k < 0) {
-				for (int j = 0; j > k; j--) {
-					ans[i] += code[(length + i - 1 + j) % length];
-				}
-			} else if (k > 0) {
-				for (int j = 0; j < k; j++) {
-					ans[i] += code[(i + 1 + j) % length];
-				}
+		if (k == 0)
+			return ans;
+
+		int width = Math.abs(k), sum = 0;
+
+		for (int i = 0; i < width; i++) {
+			sum += code[i];
+		}
+
+		for (int i = 0; i < length; i++) {
+			sum += code[(width + i) % length];
+			sum -= code[i];
+			if (k > 0) {
+				ans[i] = sum;
+			} else {
+				ans[(width + i + 1) % length] = sum;
 			}
 		}
 
