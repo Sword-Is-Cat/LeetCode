8bc05b1af36ae964a85ab56cabbdc267bd234564
jang <jhbsp@naver.com>
2024-12-01 11:19:05 +0900

    241201

--- LeetCode/src/Easy/no1346/Solution.java
+++ LeetCode/src/Easy/no1346/Solution.java
@@ -1,23 +1,20 @@
 package Easy.no1346;
 
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
 	public boolean checkIfExist(int[] arr) {
 
-		Set<Integer> oriNo = new HashSet<>();
-		Set<Integer> twiceNo = new HashSet<>();
-
+		boolean[] exist = new boolean[4001];
+		boolean[] visit = new boolean[4001];
 		for (int no : arr) {
-
-			if (oriNo.contains(no * 2) || twiceNo.contains(no))
+			if (visit[no + 2000])
+				if (no == 0)
+					return true;
+				else
+					continue;
+			if (exist[no + 2000] || exist[no * 2 + 2000])
 				return true;
-			else {
-				oriNo.add(no);
-				twiceNo.add(no * 2);
-			}
-
+			else
+				visit[no + 2000] = exist[no + 2000] = exist[no * 2 + 2000] = true;
 		}
 
 		return false;
