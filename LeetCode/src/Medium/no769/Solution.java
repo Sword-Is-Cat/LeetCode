e252b257218c721ec9d77adb0dfbac23470c36e1
jang <jhbsp@naver.com>
2024-12-19 12:13:09 +0900

    241219

--- LeetCode/src/Medium/no769/Solution.java
+++ LeetCode/src/Medium/no769/Solution.java
@@ -1,15 +1,22 @@
 package Medium.no769;
 
+import java.util.Stack;
+
 class Solution {
 	public int maxChunksToSorted(int[] arr) {
 
-		int answer = 0, max = 0;
-		for (int i = 0; i < arr.length; i++) {
-			max = Math.max(max, arr[i]);
-			if (max == i)
-				answer++;
+		Stack<int[]> stack = new Stack<>();
+
+		for (int num : arr) {
+			int[] item = new int[] { num, num };
+			while (!stack.isEmpty() && stack.peek()[1] > item[0]) {
+				int[] prev = stack.pop();
+				item[0] = Math.min(item[0], prev[0]);
+				item[1] = Math.max(item[1], prev[1]);
+			}
+			stack.push(item);
 		}
 
-		return answer++;
+		return stack.size();
 	}
 }
