075e803741f0f5e0bc8f3fc76315d80655287cd2
jang <jhbsp@naver.com>
2025-01-09 12:40:51 +0900

    250109

---
+++ LeetCode/src/Easy/no2185/Solution.java
@@ -0,0 +1,16 @@
+package Easy.no2185;
+
+class Solution {
+	public int prefixCount(String[] words, String pref) {
+
+		int cnt = 0;
+
+		for (String word : words) {
+			if (word.indexOf(pref) == 0)
+				cnt++;
+		}
+
+		return cnt;
+
+	}
+}
