c387bbd2a70be7fcf343dc5bc5a0d338bb1aa48d
jang <jhbsp@naver.com>
2025-02-06 13:08:34 +0900

    250206

--- LeetCode/src/Medium/no1726/Solution.java
+++ LeetCode/src/Medium/no1726/Solution.java
@@ -1,32 +1,22 @@
 package Medium.no1726;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 	public int tupleSameProduct(int[] nums) {
 
 		Arrays.sort(nums);
 		int ans = 0;
+		Map<Integer, Integer> map = new HashMap<>();
 
 		for (int i1 = 0; i1 < nums.length; i1++) {
 			for (int i4 = i1 + 1; i4 < nums.length; i4++) {
 
 				int prod = nums[i1] * nums[i4];
-				Set<Integer> set = new HashSet<>();
-
-				for (int i2 = i1 + 1; i2 < i4; i2++) {
-
-					int val = nums[i2];
-
-					if (prod % val == 0) {
-						if (set.contains(val))
-							ans++;
-						set.add(prod / val);
-					}
-
-				}
+				ans += map.getOrDefault(prod, 0);
+				map.put(prod, map.getOrDefault(prod, 0) + 1);
 			}
 		}
 
