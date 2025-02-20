9d3412338f8658d1f9e60e2a1ef12881fefbb4ad
jang <jhbsp@naver.com>
2025-02-20 20:22:02 +0900

    250220

--- LeetCode/src/Medium/no1980/Solution.java
+++ LeetCode/src/Medium/no1980/Solution.java
@@ -1,30 +1,13 @@
 package Medium.no1980;
 
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
 	public String findDifferentBinaryString(String[] nums) {
 
-		Set<String> set = new HashSet<>();
-		for (String num : nums) {
-			set.add(num);
-		}
-
 		StringBuilder sb = new StringBuilder();
-		int length = nums.length;
-		for (int i = 0; i < length; i++) {
-			sb.append("0");
+		for (int i = 0; i < nums.length; i++) {
+			sb.append((char) ('0' ^ '1' ^ nums[i].charAt(i)));
 		}
-		String prefix = sb.toString();
+		return sb.toString();
 
-		for (int i = 0; i <= nums.length; i++) {
-			String str = Integer.toBinaryString(i);
-			str = (prefix + str).substring(str.length());
-			if (set.add(str))
-				return str;
-		}
-
-		return null;
 	}
 }
