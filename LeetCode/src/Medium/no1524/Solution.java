7af082bf504402ddf8f94a5e799f0059d991e009
jang <jhbsp@naver.com>
2025-02-25 19:36:57 +0900

    250225

--- LeetCode/src/Medium/no1524/Solution.java
+++ LeetCode/src/Medium/no1524/Solution.java
@@ -7,12 +7,12 @@ class Solution {
 	public int numOfSubarrays(int[] arr) {
 
 		int[] counter = { 1, 0 };
-		int sum = 0, ans = 0;
+		int mask = 0, ans = 0;
 
 		for (int num : arr) {
-			sum = (sum + num) % 2;
-			ans = (ans + counter[1 ^ sum]) % MOD;
-			counter[sum]++;
+			mask = (mask ^ num) & 1;
+			ans = (ans + counter[1 ^ mask]) % MOD;
+			counter[mask]++;
 		}
 
 		return ans;
