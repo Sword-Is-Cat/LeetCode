c9c31d0a8a8d4d0774621ff017ed26873724e4ab
jang <jhbsp@naver.com>
2025-03-04 18:46:39 +0900

    250304

---
+++ LeetCode/src/Medium/no1780/Solution.java
@@ -0,0 +1,12 @@
+package Medium.no1780;
+
+class Solution {
+	public boolean checkPowersOfThree(int n) {
+		while (n > 0) {
+			if (n % 3 > 1)
+				return false;
+			n /= 3;
+		}
+		return true;
+	}
+}
