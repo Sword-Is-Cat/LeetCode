2f2bbeaeca10efe6270cf28dbfb565e757cfe16a
jang <jhbsp@naver.com>
2025-04-03 18:31:51 +0900

    250403

---
+++ LeetCode/src/Medium/no2874/Solution.java
@@ -0,0 +1,13 @@
+package Medium.no2874;
+
+class Solution {
+	public long maximumTripletValue(int[] nums) {
+		long max1 = 0, max2 = 0, max3 = 0;
+		for (int num : nums) {
+			max3 = Math.max(max3, max2 * num);
+			max2 = Math.max(max2, max1 - num);
+			max1 = Math.max(max1, num);
+		}
+		return Math.max(max3, 0);
+	}
+}
