1220ea8ca94c255c996d849ea1eb6f91336fbe36
jang <jhbsp@naver.com>
2025-05-24 14:03:23 +0900

    250524

---
+++ LeetCode/src/Easy/no2942/Solution.java
@@ -0,0 +1,15 @@
+package Easy.no2942;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+	public List<Integer> findWordsContaining(String[] words, char x) {
+		List<Integer> answer = new ArrayList<>();
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].indexOf(x) > -1)
+				answer.add(i);
+		}
+		return answer;
+	}
+}
