b67a449e4ff1024c42bbb4ca7d14bdb5deb15d95
jang <jhbsp@naver.com>
2025-05-25 09:54:41 +0900

    250525

--- LeetCode/src/Medium/no2131/Solution.java
+++ LeetCode/src/Medium/no2131/Solution.java
@@ -1,54 +1,25 @@
 package Medium.no2131;
 
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Solution {
 	public int longestPalindrome(String[] words) {
 
-		HashMap<String, Integer> counter = new HashMap<>();
+		int[][] cnt = new int[26][26];
 
 		for (String word : words) {
-			if (!counter.containsKey(word))
-				counter.put(word, 0);
-			counter.put(word, counter.get(word) + 1);
+			cnt[word.charAt(0) - 'a'][word.charAt(1) - 'a']++;
 		}
 
-		int cntPair = 0, cntDblPalin = 0;
-		boolean singlePalin = false;
-		HashSet<String> worked = new HashSet<>();
-
-		for (String word : counter.keySet()) {
-
-			if (worked.contains(word))
-				continue;
-
-			if (isPalin(word)) {
-
-				int cnt = counter.get(word);
-				cntDblPalin += cnt / 2;
-				singlePalin |= cnt % 2 == 1;
-
-			} else {
-
-				String rev = reverseString(word);
-				if (counter.containsKey(rev)) {
-
-					cntPair += Math.min(counter.get(word), counter.get(rev));
-					worked.add(rev);
-
-				}
+		int countPair = 0, centerWord = 0;
+		for (int i = 0; i < cnt.length; i++) {
+			if (cnt[i][i] % 2 == 1)
+				centerWord = 2;
+			countPair += cnt[i][i] / 2;
+			for (int j = i+1; j < cnt[i].length; j++) {
+				countPair += Math.min(cnt[i][j], cnt[j][i]);
 			}
 		}
 
-		return cntPair * 4 + cntDblPalin * 4 + (singlePalin ? 2 : 0);
+		return countPair * 4 + centerWord;
 	}
 
-	public boolean isPalin(String str) {
-		return str.charAt(0) == str.charAt(1);
-	}
-
-	public String reverseString(String str) {
-		return new StringBuilder(str).reverse().toString();
-	}
 }
