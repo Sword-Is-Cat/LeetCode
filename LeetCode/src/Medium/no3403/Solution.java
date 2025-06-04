7c0a66fc0b456e0d785f696dbb57adfbb0686bed
jang <jhbsp@naver.com>
2025-06-04 13:00:54 +0900

    250604

--- LeetCode/src/Medium/no3403/Solution.java
+++ LeetCode/src/Medium/no3403/Solution.java
@@ -1,39 +1,26 @@
 package Medium.no3403;
 
-import java.util.ArrayDeque;
-
 class Solution {
 	public String answerString(String word, int numFriends) {
 
 		if (numFriends == 1)
 			return word;
 
-		int wordLength = word.length() - numFriends + 1;
-
-		ArrayDeque<Integer> prev, curr = new ArrayDeque<>();
-		for (int i = 0; i < word.length(); i++)
-			curr.offer(i);
-		char temp = 'a';
-
-		for (int length = 0; length < wordLength && curr.size() > 1; length++) {
-			temp = 'a';
-			prev = curr;
-			curr = new ArrayDeque<>();
-			while (!prev.isEmpty()) {
-				int idx = prev.pop();
-				if (idx + length < word.length()) {
-					char ch = word.charAt(idx + length);
-					if (temp < ch) {
-						temp = ch;
-						curr.clear();
-					}
-					if (temp == ch)
-						curr.offer(idx);
-				}
+		int i = 0, j = 1, length = word.length();
+		while (j < length) {
+			int k = 0;
+			while (j + k < length && word.charAt(i + k) == word.charAt(j + k)) {
+				k++;
+			}
+			if (j + k < length && word.charAt(i + k) < word.charAt(j + k)) {
+				int t = i;
+				i = j;
+				j = Math.max(j + 1, t + k + 1);
+			} else {
+				j = j + k + 1;
 			}
 		}
 
-		int idx = curr.pop();
-		return word.substring(idx, Math.min(word.length(), idx + wordLength));
+		return word.substring(i, Math.min(word.length(), i + length - numFriends + 1));
 	}
 }
