87487800974e3fd64ed1bfb422013746476633e6
jang <jhbsp@naver.com>
2025-08-16 13:26:00 +0900

    250816

--- LeetCode/src/Easy/no1323/Solution.java
+++ LeetCode/src/Easy/no1323/Solution.java
@@ -3,15 +3,15 @@ package Easy.no1323;
 class Solution {
 	public int maximum69Number(int num) {
 
-		char[] arr = Integer.toString(num).toCharArray();
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == '6') {
-				arr[i] = '9';
-				break;
+		int MOD = 0, temp = 1, n = num;
+		while (n > 1) {
+			if (n % 10 == 6) {
+				MOD = temp;
 			}
+			temp *= 10;
+			n /= 10;
 		}
 
-		return Integer.parseInt(new String(arr));
+		return MOD == 0 ? num : (num / (MOD * 10) * (MOD * 10) + MOD * 9 + num % MOD);
 	}
 }
