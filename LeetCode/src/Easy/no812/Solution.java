24ced35464f94e7eeb9e24c128dc768ca5331c65
jang <jhbsp@naver.com>
2025-09-27 17:27:33 +0900

    250927

--- LeetCode/src/Easy/no812/Solution.java
+++ LeetCode/src/Easy/no812/Solution.java
@@ -6,14 +6,21 @@ class Solution {
 		for (int i = 0; i < points.length; i++) {
 			for (int j = i + 1; j < points.length; j++) {
 				for (int k = j + 1; k < points.length; k++) {
-					answer = Math.max(answer, areaTriangle(points[i], points[j], points[k]));
+					answer = Math.max(answer, area(points[i], points[j], points[k]));
 				}
 			}
 		}
 		return answer;
 	}
 
-	public double areaTriangle(int[] P, int[] Q, int[] R) {
-		return 0.5 * Math.abs(P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1] - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]);
+	public double area(int[]... points) {
+		int length = points.length;
+		double result = 0;
+		for (int i = 0; i < length; i++) {
+			int[] p1 = points[i], p2 = points[(i + 1) % length];
+			result += p1[0] * p2[1] - p1[1] * p2[0];
+		}
+
+		return Math.abs(result / 2);
 	}
 }
