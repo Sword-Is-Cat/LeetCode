2e7c847f1103b3bcfeca7f5d4273f19d3c4c4f76
jang <jhbsp@naver.com>
2025-10-06 12:19:28 +0900

    251006

--- LeetCode/src/Hard/no778/Solution.java
+++ LeetCode/src/Hard/no778/Solution.java
@@ -6,34 +6,32 @@ import java.util.PriorityQueue;
 class Solution {
 	public int swimInWater(int[][] grid) {
 
-		int row = grid.length, col = grid[0].length;
-		boolean[][] visit = new boolean[row][col];
+		int len = grid.length;
+		boolean[][] visit = new boolean[len][len];
 		PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparing(a -> grid[a[0]][a[1]]));
 		int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
 		queue.offer(new int[] { 0, 0 });
+		visit[0][0] = true;
 		int answer = 0;
 
-		while (!visit[row - 1][col - 1]) {
+		while (!visit[len - 1][len - 1]) {
 
-			int[] coord = queue.poll();
-			int r = coord[0], c = coord[1];
-
-			if (visit[r][c])
-				continue;
+			int[] co = queue.poll();
+			int r = co[0], c = co[1];
 
 			answer = Math.max(answer, grid[r][c]);
-			visit[r][c] = true;
 
 			for (int d = 0; d < dir.length; d++) {
 
 				int nr = r + dir[d][0], nc = c + dir[d][1];
-				if (0 <= nr && nr < row && 0 <= nc && nc < col && !visit[nr][nc]) {
+				if (0 <= nr && nr < len && 0 <= nc && nc < len && !visit[nr][nc]) {
 					queue.offer(new int[] { nr, nc });
+					visit[nr][nc] = true;
 				}
 			}
 		}
 
-		return answer;
+		return Math.max(answer, grid[len - 1][len - 1]);
 	}
 }
