5a100ea9b0787e1be469d24dfebb6374a515fd69
jang <jhbsp@naver.com>
2025-11-18 19:17:02 +0900

    251118

--- LeetCode/src/Easy/no717/Solution.java
+++ LeetCode/src/Easy/no717/Solution.java
@@ -1,17 +1,14 @@
 package Easy.no717;
 
 class Solution {
-    public boolean isOneBitCharacter(int[] bits) {
-        for(int i = 0;i<bits.length;i++){
-            if(bits[i]==1){
-                if(i+1 == bits.length){
-                    return false;
-                }
-                i++;
-            }else if(i+1 == bits.length){
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean isOneBitCharacter(int[] bits) {
+		for (int i = 0; i < bits.length; i++) {
+			if (bits[i] == 1) {
+				i++;
+			} else if (i + 1 == bits.length) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
