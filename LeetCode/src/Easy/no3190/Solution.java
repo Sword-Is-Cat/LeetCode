56ea0bef96c0290014d20c017d1bec2e51f51eee
jang <jhbsp@naver.com>
2025-11-22 12:47:35 +0900

    251122

---
+++ LeetCode/src/Easy/no3190/Solution.java
@@ -0,0 +1,12 @@
+package Easy.no3190;
+
+class Solution {
+	public int minimumOperations(int[] nums) {
+		int ans = 0;
+		for (int num : nums) {
+			if (num % 3 > 0)
+				ans++;
+		}
+		return ans;
+	}
+}
