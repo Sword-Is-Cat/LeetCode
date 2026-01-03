17dd2fd66dd21fec77c65304a08776b2a9a29c68
jang <jhbsp@naver.com>
2026-01-03 11:54:54 +0900

    260103

--- LeetCode/src/Hard/no1411/Solution.java
+++ LeetCode/src/Hard/no1411/Solution.java
@@ -5,13 +5,13 @@ class Solution {
 
 		final int MOD = 1_000_000_007;
 
-		long[] curr = { 6, 6 }, prev;
+		long ryr = 6, ryg = 6;
 		while (--n > 0) {
-			prev = curr;
-			curr = new long[2];
-			curr[0] = (3 * prev[0] + 2 * prev[1]) % MOD;
-			curr[1] = (2 * prev[0] + 2 * prev[1]) % MOD;
+			long newRyr = (3 * ryr + 2 * ryg) % MOD;
+			long newRyg = (2 * ryr + 2 * ryg) % MOD;
+			ryr = newRyr;
+			ryg = newRyg;
 		}
-		return (int) ((curr[0] + curr[1]) % MOD);
+		return (int) ((ryr + ryg) % MOD);
 	}
 }
